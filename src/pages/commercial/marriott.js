e3b4db8567301e348737cc500d117d8b8e089fc2
Sarah Carter <carters@transblue.org>
2021-08-17 14:16:27 -0700

    added global a.cta and button.cta styles from snow website. Added locations link and contactModal to commercial Marriot component

--- src/pages/commercial/marriott.js
+++ src/pages/commercial/marriott.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactModal from '../../components/contactModal';
 import asphalt from '../../images/multifamily/asphalt.jpg';
 import './style.css';
 
@@ -13,9 +14,10 @@ function Marriott() {
                     <h4 className='section-header'>THE MARRIOTT PROJECT</h4>
                     <p className='section-p'>Asphalt maintenence is essential in commercial properties. Applying a seal coat overlay to the existing asphalt keeps the parking lot looking fresh and sharp.</p>
                     <p className='section-p'>When working with a worldwide hospitality client such as Marriott, it is crucial to be on time, on budget, and have minimal impact on guests.</p>
-                    <p className='section-p pb-4'>The service was delivered with WorldClass efficiency and established Transblue as a reliable partner for Marriott's future needs.</p>
+                    <p className='section-p'>The service was delivered with WorldClass efficiency and established Transblue as a reliable partner for Marriott's future needs.</p>
+                    <a className='cta' href='/locations'>FIND A LOCATION</a>
+                    <ContactModal />
                 </div>
-
             </div>
         </div>
     )
