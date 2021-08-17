16d0caa96879cb2df5bad3a0008a16f6a93a81a1
Sarah Carter <carters@transblue.org>
2021-08-17 16:28:11 -0700

    added slideshow to hospitality component on commercial page

--- src/pages/commercial/about.js
+++ src/pages/commercial/about.js
@@ -1,21 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import Fade from 'react-reveal/Fade';
 import commercial from '../../images/commercial/commercial2.jpeg';
-import snow from '../../images/commercial/plow.jpg';
-import asphalt from '../../images/commercial/asphalt.jpg';
-import ev from '../../images/commercial/ev.jpg';
 import './style.css';
 
-const images = [commercial, snow, asphalt, ev]
-
 function About() {
-    const [index, setIndex] = useState(0);
-
-    useEffect(() => {
-        setTimeout(() => {
-            index < images.length -1 ? setIndex(index + 1) : setIndex(0)
-        }, 5000)
-    })
     return(
         <div className='container commercial-about'>
             <div className='row'>
@@ -25,12 +13,7 @@ function About() {
                     <p className='section-p pb-4'>Transblue offers snow hauling, paving, and more for commercial clients, letting you put your attention where it belongs: on your customers.</p>
                 </div>
                 <div className='col-lg-6'>
-                    <div className='about-slideshow'>
-                        {index === 0 && <Fade opposite duration={1000} ><img src={images[index]} alt='commercial landscaping' /></Fade>}
-                        {index === 1 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-                        {index === 2 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-                        {index === 3 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-                    </div>
+                   <img src={commercial} alt='' />
                 </div>
             </div>
         </div>
--- src/pages/commercial/hospitality.js
+++ src/pages/commercial/hospitality.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Fade from 'react-reveal/Fade';
+import snow from '../../images/commercial/plow.jpg';
+import asphalt from '../../images/commercial/asphalt.jpg';
+import ev from '../../images/commercial/ev.jpg';
 import check from '../../images/checkorange.png';
 import './style.css';
 
 function Hospitality() {
+    const images = [snow, asphalt, ev];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            index < images.length -1 ? setIndex(index + 1) : setIndex(0)
+        }, 5000)
+    })
     return(
         <div className='hospitality-wrapper container-fluid'>
             <div className='row'>
-                <div className='col-12 col-lg-6'>
-                    <h4 className='section-header'>
-                        NO MATTER THE PROJECT,<br />
+                <div className='col-12 col-lg-6 pr-5 mt-3'>
+                    <h6>NO MATTER THE PROJECT,</h6>
+                    <h4 className='section-header hospitality-header'>
                         YOU CAN COUNT ON TRANSBLUE
                     </h4>
+                    {/* <p className='section-p mb-1'>We're a one-stop shop for all your landscaping needs. Lorem ipsum dolor sit amet.</p> */}
                     <p className='hospitality-subheader'>
                         <img src={check} className='check-icon' /> WE NEVER CUT CORNERS
                     </p>
@@ -19,10 +32,18 @@ function Hospitality() {
                         <img src={check} className='check-icon' /> WE ALWAYS GO ABOVE &amp; BEYOND
                     </p>
                     <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis sed odio sed lobortis. Pellentesque.</p>
-                    <p className='hospitality-subheader'>
+                    {/* <p className='hospitality-subheader'>
                         <img src={check} className='check-icon' /> WE'RE A ONE-STOP SHOP FOR ALL YOUR NEEDS
                     </p>
-                    <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis sed odio sed lobortis. Pellentesque.</p>
+                    <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis sed odio sed lobortis. Pellentesque.</p> */}
+                </div>
+                <div className='col-12 col-lg-6'>
+                    <div className='about-slideshow'>
+                        {index === 0 && <Fade opposite duration={1000} ><img src={images[index]} alt='commercial landscaping' /></Fade>}
+                        {index === 1 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
+                        {index === 2 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
+                        {index === 3 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
+                    </div>
                 </div>
             </div>
         </div>
