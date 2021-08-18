28db6fe9743cf29c16fcd7ac39b7affca42afb0d
Sarah Carter <carters@transblue.org>
2021-08-17 17:20:36 -0700

    modified css styling of landing header title to match other pages. Removed animation of font

--- src/components/landing/header/index.js
+++ src/components/landing/header/index.js
@@ -5,7 +5,7 @@ function Header() {
             <div className='header'>
                 <div id='header_slide'>
                     <div className='header-title centered-text'>
-                        {/* Luxury Exterior Design */}
+                        LUXURY EXTERIOR DESIGN
                     </div>
                     {/* <h1>Luxury Exterior Design</h1> */}
                     <div id='header-overlay'></div>
