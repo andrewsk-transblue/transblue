e0c9f614cc8f4bb4c2ad2a421ac4ad07c15f8d3e
Sarah Carter <carters@transblue.org>
2021-08-18 10:30:58 -0700

    converted LocationList functional component to a class component

--- src/pages/locations/locationList.js
+++ src/pages/locations/locationList.js
@@ -1,87 +1,90 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 //import { useEasybase } from 'easybase-react';
 //import locationsDb from './db.js';
 import comingSoon from './comingSoon';
 import './style.css';
 
-import locationsDb from './db.js';
+//import locationsDb from './db.js';
 const geolib = require('geolib');
 
 
-function LocationList(props) {
-    const [isLoading, setIsLoading] = useState(true);
-
-    const locations = props.locations;
-    const locationList = [];
-
-    console.log(locations);
-
-    useEffect(() => {
-        getLocations()
-    }, [props.radius, props.coords])
-
-    function getLocations() {
-        let userLocation = {
-            latitude: props.coords[0],
-            longitude: props.coords[1]
-        }
-        for(let i=0; i < locations.length; i++) {
-            let franchiseLocation = {
-                latitude: locations[i].lat,
-                longitude: locations[i].lon
-            }
-            let distance = geolib.getDistance(userLocation, franchiseLocation) / 1600;
-            if(distance < props.radius) {
-                locationList.push(locations[i])
-            }
-        }
-        console.log(locationList)
+class LocationList extends Component {
+    state={
+        isLoading: true,
+        locationList: []
     }
 
-    return(
-        <div className='container-fluid location-list-container'>
-            <div className='no-locations'>
-                NO LOCATIONS AVAILABLE <br />
-                <small>Please try different search criteria</small>
-            </div>
-             {locationList.map((location, index) => {
-                return(
-                    <div className='row location' >
-                        <div className='col-lg-1 col-1 pl-0 pt-3 index'>{index + 1}</div>
-                        <div className='col-lg-11 col-11 pl-1 py-3'>
-                            <p>{location.name.toUpperCase()}</p>
-                            <div className='location-body'>
-                                {location.address1}, {location.address2}<br />
-                                {location.city}, {location.state} {location.zipcode}<br />
-                                <div className='location-contact'>
-                                    <i className="fas fa-phone"></i>{location.phone}<br />
-                                    <i className="fas fa-envelope"></i>{location.email}<br /> 
-                                </div>                               
-                                <span>
-                                    <a href={`/locations/${location.state}/${location.urlCity}`} target='_blank' rel="noreferrer"><button>Website</button></a>
-                                    <button onClick={() => props.selectLocation(location.lat, location.lon)} id='view-location'>View On Map</button>
-                                </span><br />
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-            {/* render list of all locations coming soon in the state searched */}
-            {/* {comingSoon.map(location => {
-                if(props.state === location.state) {
+    componentDidMount = () => {
+        this.getLocationList()
+    }
+
+    getLocationList = () => {
+        let locationList = [];
+        let userLocation = {
+            latitude: this.props.coords[0],
+            longitude: this.props.coords[1]
+        }
+        for(let i=0; i < this.props.locations.length; i++) {
+            let franchiseLocation = {
+                latitude: this.props.locations[i].lat,
+                longitude: this.props.locations[i].lon
+            }
+            let distance = geolib.getDistance(userLocation, franchiseLocation) / 1600;
+            if(distance < this.props.radius) {
+                locationList.push(this.props.locations[i])
+            }
+        }
+        this.setState({locationList: locationList})
+    }
+
+    render() {
+        console.log(this.props)
+        return(
+            <div className='container-fluid location-list-container'>
+                {this.state.locationList.length === 0 && <div className='no-locations'>
+                    NO LOCATIONS AVAILABLE <br />
+                    <small>Please try different search criteria</small>
+                </div>}
+                {this.state.locationList.length > 0 && this.state.locationList.map((location, index) => {
+                    //console.log(location)
                     return(
-                        <div className='row location'>
-                            <div className='col-lg-12'>
-                                <b>Transblue {location.city}</b>
-                                <br />
-                                <div className='coming-soon'>Coming Soon!</div>
+                        <div className='row location' >
+                            <div className='col-lg-1 col-1 pl-0 pt-3 index'>{index + 1}</div>
+                            <div className='col-lg-11 col-11 pl-1 py-3'>
+                                <p>{location.name.toUpperCase()}</p>
+                                <div className='location-body'>
+                                    {location.address1}, {location.address2}<br />
+                                    {location.city}, {location.state} {location.zipcode}<br />
+                                    <div className='location-contact'>
+                                        <i className="fas fa-phone"></i>{location.phone}<br />
+                                        <i className="fas fa-envelope"></i>{location.email}<br /> 
+                                    </div>                               
+                                    <span>
+                                        <a href={`/locations/${location.state}/${location.urlCity}`} target='_blank' rel="noreferrer"><button>Website</button></a>
+                                        <button onClick={() => this.props.selectLocation(location.lat, location.lon)} id='view-location'>View On Map</button>
+                                    </span><br />
+                                </div>
                             </div>
                         </div>
-                    )}
-                else return null;
-            })} */}
-        </div>
-    )
+                    )
+                })}
+                {/* render list of all locations coming soon in the state searched */}
+                {/* {comingSoon.map(location => {
+                    if(props.state === location.state) {
+                        return(
+                            <div className='row location'>
+                                <div className='col-lg-12'>
+                                    <b>Transblue {location.city}</b>
+                                    <br />
+                                    <div className='coming-soon'>Coming Soon!</div>
+                                </div>
+                            </div>
+                        )}
+                    else return null;
+                })} */}
+            </div>
+        )
+    }
 }
 
 export default LocationList;
