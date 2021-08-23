bd5b5545939e4ec08f54d4f531d1c480eee80e7e
Sarah Carter <carters@transblue.org>
2021-08-23 10:35:13 -0700

    added text to multifamily camp component from pastor dave

--- src/pages/multifamily/index.js
+++ src/pages/multifamily/index.js
@@ -16,6 +16,7 @@ import parkinglot from '../../images/multifamily/asphalt.jpg';
 import snow from '../../images/multifamily/snow.jpg';
 import luxury from '../../images/multifamily/luxury.jpg';
 import './style.css';
+import ContactModal from '../../components/contactModal';
 
 function Multifamily() {
     return(
@@ -75,7 +76,7 @@ function Multifamily() {
                                 <li>Manage operational expenses to remain within budget</li>
                                 <li>Provide a safe, comfortable environment for staff, residents, and visitors</li>
                             </ul>
-                            <a className='cta' href='/locations'>FIND A LOCATION</a>
+                            <a className='cta' href='/locations'>FIND A LOCATION</a><ContactModal />
                         </div>
                         <div className='col-lg-6'>
                             <img className='pt-4' src={luxury} alt='Luxury Apartment' />
@@ -102,10 +103,13 @@ function Multifamily() {
                     <div className='row'>
                         <div className='col-lg-6 order-lg-12'>
                             <h4 className='section-header'>TRANSBLUE SUPPORTS KIDS CAMPS</h4>
-                            <div className='section-p mb-3'>Summer is a great time for camps. Camps are transformational in the lives of children and teens. Solid Rock Mission, one of our mBridge partners, hosts camps all summer long for thousands of kids and teens (1782 kids in June).</div>
+                            {/* <div className='section-p mb-3'>Summer is a great time for camps. Camps are transformational in the lives of children and teens. Solid Rock Mission, one of our mBridge partners, hosts camps all summer long for thousands of kids and teens (1782 kids in June).</div>
                             <div className='section-p mb-3'>This month we have had back to back to back camps. We organized camps in villages and small towns. Over 100 kids showed up to each camp with 1782 attending in June. These kids come from various families and backgrounds with all sorts of hurts and broken stories. Our goal is to always and foremost show them God's love.</div>
-                            {/* <p>One of the stations we have at camp is music station. Here the kids learn to sing praise songs which they memorize and take home with them. We believe that the message of these songs has the power to impact these children with hope and love.</p> */}
-                            <div className='section-p pb-4'>Most of the kids that attended these camps over the course of this month come from broken families. Many of these families lost their loved ones while others had parents that simply left their family. At camp, it is a great opportunity for us to love them and most of all show them God's love.</div>
+                            <div className='section-p pb-4'>Most of the kids that attended these camps over the course of this month come from broken families. Many of these families lost their loved ones while others had parents that simply left their family. At camp, it is a great opportunity for us to love them and most of all show them God's love.</div> */}
+
+                            <div className='section-p mb-3'>Summer is a great time for camps. Camps are transformational in the lives of children and teens.</div>
+                            <div className='section-p mb-3'>Solid Rock Mission, one of our mBridge partners, reaches into rural and war torn regions of southern and eastern Ukraine.</div>
+                            <div className='section-p'>Alcoholism and the break down of the family is rampant. Children and youth are often victims of abuse or politically radicalized. Last year, Transblue helped Solid Rock Missions send over 4000 children and youth to attend summer camps with over 1000 committing their lives to Christ.</div>
                         </div>
                         <div className='col-lg-6'>
                             <CampImages />
