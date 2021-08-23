fb1a629b9490837d73a43f9b3a1ce024849c05b7
Sarah Carter <carters@transblue.org>
2021-08-23 10:24:49 -0700

    added window scroll event listener to contact cta. Only show when user scrolls past 400px on screen

--- src/components/contactCta/index.js
+++ src/components/contactCta/index.js
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Form from './form';
 import Slide from 'react-reveal/Slide';
 import './style.css';
 
 function ContactCta() {
     const [displayForm, setDisplayForm] = useState(false);
+    const [displayCTA, setDisplayCTA] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 400) setDisplayCTA(true)
+            else setDisplayCTA(false)
+        })
+    })
     return(
-        <div className='contact-cta'>
+        displayCTA && <div className='contact-cta'>
             {!displayForm && <button id='cta' onClick={() => setDisplayForm(!displayForm)}>GET MORE INFO</button>}
             {displayForm && 
                 <Slide right>
