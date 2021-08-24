20146725735068c02665b7a2f82f06c702ea2a0d
Sarah Carter <carters@transblue.org>
2021-08-24 10:16:05 -0700

    created toggle buttons that display and hide locations map

--- src/pages/locations/index.js
+++ src/pages/locations/index.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment } from 'react';
+import React, {useState, Fragment } from 'react';
 import Navbar from '../../components/navbar/index';
 import Header from '../../components/header';
 import Footer from '../../components/footer/index';
@@ -12,21 +12,24 @@ import './style.css';
 
 //import locations from './db';
 
-class Locations extends Component {
+function Locations(props) {
+        const [viewMap, setViewMap] = useState(false);
    
-    render() {
         return(
             <Fragment>
             <Navbar page='LOCATIONS' />
             <Header img={locationsHeader} title='FIND A LOCATION' subtitle='' />
             <div className='map-view-toggle'>
-                <i className="fas fa-list-ul"></i>LIST VIEW <i className="fas fa-map"></i>MAP VIEW<br />
+                <button onClick={() => setViewMap(false)} className={!viewMap ? 'active' : ''}>
+                    <i className="fas fa-list-ul"></i>LIST VIEW</button> 
+                <button onClick={() => setViewMap(true)} className={viewMap ? 'active' : ''}>
+                    <i className="fas fa-map"></i>MAP VIEW
+                </button>
             </div>
-            <MapContainer locations={this.props.locations} />
+            {viewMap && <MapContainer locations={props.locations} />}
             <Footer locationPage={true} />
         </Fragment>
         )
-    }
 
 }
 
