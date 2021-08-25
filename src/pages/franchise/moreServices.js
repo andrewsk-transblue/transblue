330895b077dc613294ee6c6790024b9fd677c4e5
Sarah Carter <carters@transblue.org>
2021-08-25 14:59:43 -0700

    created a new more services section

--- src/pages/franchise/moreServices.js
+++ src/pages/franchise/moreServices.js
@@ -1,4 +1,5 @@
 import React, {useState, Fragment} from 'react';
+//import line from '../../images/lineblack.PNG';
 
 function MoreServices() {
     const [displayServices, setDisplayServices] = useState(false);
@@ -11,7 +12,7 @@ function MoreServices() {
         <div className='row'>
             {!displayServices ? <button className='cta' onClick={() => toggle(true)}>MORE SERVICES</button> : <button className='cta' onClick={() => toggle(false)}>LESS SERVICES</button>}
         </div>
-        {displayServices && <div className='row more-services'>
+        {/* {displayServices && <div className='row more-services'>
         <div className='col-lg-3'>
             Concrete<br />
             Patios<br />
@@ -29,7 +30,6 @@ function MoreServices() {
             Outdoor Kitchens<br />
         </div>
         <div className='col-lg-3'>
-            {/* Sports Courts<br /> */}
             Firepits<br />
             Paving<br />
             Striping<br />
@@ -43,7 +43,75 @@ function MoreServices() {
             Siding<br />
             Solar Panels<br />
         </div>
-    </div>}
+    </div>} */}
+
+    <div className='row more-services card-deck mx-0'>
+        <div className='col-lg-6 card'>
+            <h5 className="line-header">
+                {/* <img src={line} alt='' />  */}
+                GENERAL CONTRACTING
+            </h5>
+            <div className='row'>
+                <div className='col-lg-4 section-p'>
+                    Concrete
+                    Patios
+                    Flagstone
+                    Retaining Walls
+                    Structural Support
+                    Landscape Design
+                </div>
+                <div className='col-lg-4 section-p'>
+                    Fences
+                    Gates
+                    Outdoor Kitchens
+                    Firepits
+                    Paving
+                    Crack Seal
+                </div>
+                <div className='col-lg-4 section-p'>
+                    Roofing
+                    Siding
+                    Striping
+                    Synthetic Turf
+                    Slope Creep
+                </div>
+            </div>
+            <div className='card-text'>
+                <a className='cta' href='/'>GC WEBSITE</a>
+            </div>
+        </div>
+        <div className='col-lg-3 card'>
+            <h5>SNOW SERVICES</h5>
+            <div className='row'>
+                <div className='col-lg-12 section-p'>
+                    Snow Hauling<br />
+                    De-icing<br />
+                    Plowing<br />
+                    Truck top clearing<br />
+                    Snow Melting
+                </div>
+            </div>
+            <div className='card-text'>
+                <a className='cta' href='/'>SNOW WEBSITE</a>
+            </div>
+        </div>
+        <div className='col-lg-3 card'>
+            <h5>GREEN SERVICES</h5>
+            <div className='row'>
+                <div className='col-lg-12 section-p'>
+                    Solar Panels<br />
+                    Sustainable Roofs<br />
+                    EV Installation<br />
+                    Shade Structures<br />
+                    Xeriscapes
+                </div>
+            </div>
+            <div className='card-text'>
+                <a className='cta' href='/'>GREEN WEBSITE</a>
+            </div>
+        </div>
+    </div>
+    
     </Fragment>
     )
 }
