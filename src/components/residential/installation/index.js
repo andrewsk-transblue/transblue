de4214847ad582c4fa407afeaead62b113080f43
Sarah Carter <carters@transblue.org>
2021-08-27 16:58:53 -0700

    added an image to installation component

--- src/components/residential/installation/index.js
+++ src/components/residential/installation/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import installation from '../../../images/residential/installation.jpeg';
 import './style.css';
 
 function Installation() {
     return(
         <div className='installation-wrapper container-fluid'>
             <div className='row'>
-                <div className='col-12 col-lg-6'></div>
+                <div className='col-12 col-lg-6'>
+                    <img src={installation} alt='' />
+                </div>
                 <div className='col-12 col-lg-6 left-text'>
                     <h4 className='section-header'>DESIGN, INSTALLATION, CONSTRUCTION</h4>
                     <p className='section-p'>
-                        At Transblue, our team of landscape architects and designers and put your plans to paper and design a beautiful landscape that will be cost effective and add beautification to your property, no matter what the size.
+                        At Transblue, our team of landscape architects and designers and put your plans to paper to design a beautiful landscape. Our projects are  cost effective and add beautification to your property, no matter the size.
                     </p>
                     <p className='section-p'>
-                        Once the cost and plan is approved, our installation and construction team will provide a written time line and project schedule. We will stay on task and on schedule and complete the installation of your project from conception to completion.
+                        Once the cost and plan is approved, our installation and construction team will provide you with a written time line and project schedule. We stay on schedule from conception to completion.
+                        {/* We will stay on task and on schedule and complete the installation of your project from conception to completion. */}
                     </p>
                 </div>
             </div>
