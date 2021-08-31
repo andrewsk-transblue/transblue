d4b8de74ff30a56e7c9862a7e122250d8d4f00f6
Sarah Carter <carters@transblue.org>
2021-08-31 10:26:21 -0700

    added different image to multifamily services component

--- src/components/multifamily/services/index.js
+++ src/components/multifamily/services/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import parkinglot from '../../../images/multifamily/parkinglot.jpeg';
+import services from '../../../images/multifamily/services.jpeg';
 import './style.css';
 
 function ServiceList() {
@@ -7,23 +8,23 @@ function ServiceList() {
         <div className='servicelist-wrapper container-fluid'>
             <div className='row'>
                 <div className='col-12 col-lg-6'>
-                    <img src={parkinglot} alt='' />
+                    <img src={services} alt='' />
                 </div>
                 <div className='col-12 col-lg-6'>
                     <h4 className='section-header'>WE OFFER A VARIETY OF SERVICES</h4>
-                    <p className='asterisk'><span>*</span> Snow and Ice Management</p>
-                    <p className='asterisk'><span>*</span> Swimming Pool construction and resurfacing</p>
-                    <p className='asterisk'><span>*</span> Asphalt Paving</p>
-                    <p className='asterisk'><span>*</span> Large Landscape installs</p>
-                    <p className='asterisk'><span>*</span> Roofing</p>
-                    <p className='asterisk'><span>*</span> Siding</p>
-                    <p className='asterisk'><span>*</span> Painting</p>
-                    <p className='asterisk'><span>*</span> Remodels and refreshes</p>
-                    <p className='asterisk'><span>*</span> Concrete work</p>
-                    <p className='asterisk'><span>*</span> Drainage Systems</p>
-                    <p className='asterisk'><span>*</span> Decking, fencing and railing systems. </p>
-                    <p className='asterisk'><span>*</span> Electric Vehicle Charging Systems, Solar Power</p>
-                    <p className='asterisk'><span>*</span> ESG Service Offerings</p>
+                    <p className='asterisk section-p'><span>*</span> Snow and Ice Management</p>
+                    <p className='asterisk section-p'><span>*</span> Swimming Pool construction and resurfacing</p>
+                    <p className='asterisk section-p'><span>*</span> Asphalt Paving</p>
+                    <p className='asterisk section-p'><span>*</span> Large Landscape installs</p>
+                    <p className='asterisk section-p'><span>*</span> Roofing</p>
+                    <p className='asterisk section-p'><span>*</span> Siding</p>
+                    <p className='asterisk section-p'><span>*</span> Painting</p>
+                    <p className='asterisk section-p'><span>*</span> Remodels and refreshes</p>
+                    <p className='asterisk section-p'><span>*</span> Concrete work</p>
+                    <p className='asterisk section-p'><span>*</span> Drainage Systems</p>
+                    <p className='asterisk section-p'><span>*</span> Decking, fencing and railing systems. </p>
+                    <p className='asterisk section-p'><span>*</span> Electric Vehicle Charging Systems, Solar Power</p>
+                    <p className='asterisk section-p'><span>*</span> ESG Service Offerings</p>
                     <p className='section-p'>Give our professionals a call today to discuss your project needs</p>
                 </div>
             </div>
