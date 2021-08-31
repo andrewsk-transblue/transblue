a4f6da09ca2eaa1f070278410e1de0934385c679
Sarah Carter <carters@transblue.org>
2021-08-31 14:58:16 -0700

    added new asphalt image to commercial refresh component

--- src/pages/commercial/refresh.js
+++ src/pages/commercial/refresh.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import paint from '../../images/commercial/paint.jpg';
 import roof from '../../images/commercial/roof.jpg';
+import asphalt from '../../images/commercial/asphalt2.jpg';
 import './style.css';
 
 function Refresh() {
@@ -11,17 +12,17 @@ function Refresh() {
         <hr className='mb-4' />
         <div className='row'>
             <div className='col-lg-4 col-md-6'>
-                <img src={paint} alt='Fresh Paint' />
+                <img src={asphalt} alt='Asphalt Services' />
                 <h6 className='mt-4 mb-3'>ASPHALT SERVICES</h6>
                 <p className='section-p mb-4'>We will assess your property's current condition and provide a quote to patch, pave, restore, mill, sealcoat or stripe your existing parking lot areas. We offer full-service paving for new construction. We also offer overlaying services for parking lots with excessive damaged asphalt.</p>
             </div>
             <div className='col-lg-4 col-md-6'>
-                <img src={roof} alt='Fresh Paint' />
+                <img src={roof} alt='Roofing Services' />
                 <h6 className='mt-4 mb-3'>ROOFING SERVICES</h6>
                 <p className='section-p mb-4'>At Transblue we provide commercial roofing services, including stripping your old roofing system, making structural repairs as required, and re-roofing with roofing material chosen by you. We install metals, asphalt shingles, green roofs, tiles, torch down, and systems such as flat PVC and TPO.</p>
             </div>
             <div className='col-lg-4 col-md-6'>
-                <img src={paint} alt='Fresh Paint' />
+                <img src={paint} alt='Brand Refresh' />
                 <h6 className='mt-4 mb-3'>BRAND REFRESHES</h6>
                 <p className='section-p'>Is your brand looking tired and in need of a face lift? Transblue can transform the exterior with a new design, façade, construction and paint! Ready for the inside as well? We offer new carpet, tiles, paint, and complete overhauls.</p>
             </div>
