cf51dd534a91815cc0eb53fae6f921596a623d16
Sarah Carter <carters@transblue.org>
2021-08-31 11:11:39 -0700

    redesigned franchise professional component

--- src/pages/franchise/professional.js
+++ src/pages/franchise/professional.js
@@ -1,40 +1,44 @@
 import React from 'react';
+import ContactModal from '../../components/contactModal';
 import './professional.css';
 
 function Professional() {
     return(
         <div className='professional-wrapper'>
             <div className='container-fluid'>
+                <h3>OUR PROMISE TO YOU</h3>
+                <p className='section-p'>We are 100% dedicated to customer satisfaction</p>
                 <div className='row'>
-                    <div className='col-12 col-lg-4'>
+                    <div className='col-12 col-lg-4 text-center'>
+                        <i className="fas fa-seedling fa-2x"></i>
                         <h5>
-                            <i className="fas fa-seedling"></i> OUTSTANDING SERVICE                 
+                             OUTSTANDING SERVICE              
                             {/* <svg viewBox="0 0 100 100" width="25">
                                 <path d="M25,2 L2,2 L2,25" fill="none" stroke="black" stroke-width="3" />
                                 <path d="M75,98 L98,98 L98,75" fill="none" stroke="black" stroke-width="3" />
                             </svg> */}
                         </h5>
-                        <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
+                        <p className='section-p body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
                     </div>
-                    <div className='col-12 col-lg-4'>
-                        {/* <svg viewBox="0 0 100 100" width="50">
-                            <text x="50%" y="50%">
-                                <i className="fas fa-seedling fa-2x"></i>
-                            </text>
-                            <path d="M25,2 L2,2 L2,25" fill="none" stroke="black" stroke-width="3" />
-                            <path d="M75,98 L98,98 L98,75" fill="none" stroke="black" stroke-width="3" />
-                        </svg> */}
-                        <h5><i className="fas fa-seedling"></i> PROFESSIONAL WORK</h5>
-                        <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
+                    <div className='col-12 col-lg-4 text-center'>
+                        <i className="fas fa-seedling fa-2x"></i>
+                        <h5> PROFESSIONAL WORK</h5>
+                        <p className='section-p body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
                     </div>
-                    <div className='col-12 col-lg-4'>
+                    <div className='col-12 col-lg-4 text-center'>
                         {/* <svg viewBox="0 0 100 100"  width="50">
                             GREAT COMMUNICATION
                             <path d="M25,2 L2,2 L2,25" fill="none" stroke="black" stroke-width="3" />
                             <path d="M75,98 L98,98 L98,75" fill="none" stroke="black" stroke-width="3" />
                         </svg> */}
-                        <h5><i className="fas fa-seedling"></i> GREAT COMMUNICATION</h5>
-                        <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
+                        <i className="fas fa-seedling fa-2x"></i> 
+                        <h5>GREAT COMMUNICATION</h5>
+                        <p className='section-p body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue cursus sagittis. Nam vel enim.</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-lg-12 text-center'>
+                        <a className='cta' href='/locations'>FIND A LOCATION</a>
                     </div>
                 </div>
             </div>
