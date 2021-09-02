7c24caabf3d1ce457fcbbc1acbdb5e525ad9fbff
Sarah Carter <carters@transblue.org>
2021-09-01 17:02:53 -0700

    added custom tile layer to franchise map from mapbox

--- src/pages/franchise/commercial.js
+++ src/pages/franchise/commercial.js
@@ -4,6 +4,8 @@ import commercial from '../../images/franchise/commercial.jpeg';
 import './commercial.css';
 
 function Commercial(props) {
+
+    let tel = props.location.phone.replace(/[^A-Z0-9]/ig, "")
     return(
         <Section 
             img={commercial}
@@ -11,7 +13,7 @@ function Commercial(props) {
             p1={`At ${props.location.name}, our teams are focused on making your life simple and reducing the burden that comes with the capital projects required to keep your business looking its best.`}
             p2='We understand that cost and quality are the keys to success of your initiative. Our promise is to deliver a truly World Class Experience.'
             p3={`Contact us today and experience what construction should be like!<br />
-            <b>${props.location.phone}<br />
+            <b><a href={tel:+${tel}}>${props.location.phone}</a><br />
             ${props.location.email}</b>`}
             imageSide='left'
         />
--- src/pages/franchise/map.js
+++ src/pages/franchise/map.js
@@ -9,8 +9,10 @@ function Map(props) {
         <MapContainer center={position} zoom={11} scrollWheelZoom={true} id='mapid'>
             <TileLayer
                 attribution='Transblue'
-                url='https://api.mapbox.com/styles/v1/sarahmariecarter/ckq2imn0v1a7x17o3s84v62bj/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FyYWhtYXJpZWNhcnRlciIsImEiOiJja3EyaXBub2UwcDVnMm9xYjY1M282MWtsIn0.M45SX1Os1C-H17ssCoAL8g'
+                url='https://api.mapbox.com/styles/v1/sarahmariecarter/ckt259o46010e18njaz84sofg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FyYWhtYXJpZWNhcnRlciIsImEiOiJja3EyaXBub2UwcDVnMm9xYjY1M282MWtsIn0.M45SX1Os1C-H17ssCoAL8g'
+                // url='https://api.mapbox.com/styles/v1/sarahmariecarter/ckt259o46010e18njaz84sofg.html?fresh=true&title=view&access_token=pk.eyJ1Ijoic2FyYWhtYXJpZWNhcnRlciIsImEiOiJja3A4dGE2eGUwYmhhMm9ueHQxYm81aG1jIn0.IL6ap7-sSL2WXIipenrzUQ'
                 />
+                
             <Marker position={[props.lat, props.lon]} />
         </MapContainer>
     )
