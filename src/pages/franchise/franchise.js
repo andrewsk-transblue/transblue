34650015be827e1f347356c46be0b3c460f5a246
Sarah Carter <carters@transblue.org>
2021-09-07 14:06:42 -0700

    made residential about component mobile responsive

--- src/pages/franchise/franchise.js
+++ src/pages/franchise/franchise.js
@@ -62,7 +62,7 @@ function Franchise(props) {
             <div className='franchise-wrapper'>
                 <Navbar page='' franchise={true} city={`${easybaseData[0].city}`} state={`${easybaseData[0].state}`} phone={`${easybaseData[0].phone}`} name={`${easybaseData[0].name}`} urlCity={`${easybaseData[0].urlcity}`}  />
                 <Header
-                    img={`${process.env.PUBLIC_URL}/images/${easybaseData[0].image}`}
+                    // img={`${process.env.PUBLIC_URL}/images/${easybaseData[0].image}`}
                     region={easybaseData[0].region}
                     title={easybaseData[0].name.toUpperCase()}
                     subtitle={`Serving ${easybaseData[0].city}, ${easybaseData[0].state} and surrounding areas`}
--- src/pages/franchise/multifamily.js
+++ src/pages/franchise/multifamily.js
@@ -9,7 +9,7 @@ function Multifamily(props) {
             img={multifamily}
             title='BEAUTIFY YOUR MULTIFAMILY RESIDENCE'
             p1={`At ${props.location.name}, we understand that as a property manager, you are extremely busy. Our professionals make your life easier by turning proposals around quickly.`}
-            p2='Our teams attend board meetings and describe projects and services to ensure the delivery to your communities is exactly what it should be, World Class. Transblue is an expert in the Capital Projects that your communities’ budget for each year.'
+            p2='Our teams attend board meetings and describe projects and services to ensure the delivery to your communities is exactly what it should be, World Class. Transblue is an expert in the Capital Projects that your community budgets for each year.'
             p3=''
             contact={{
                 phone: props.location.phone,
