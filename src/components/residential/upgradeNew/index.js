60d3a7b5e2ce09cebcbf47d802edcc2aaed8746e
Sarah Carter <carters@transblue.org>
2021-09-07 11:30:22 -0700

    made upgradeNew component mobile responsive, cropped image for smaller screens

--- src/components/residential/upgradeNew/index.js
+++ src/components/residential/upgradeNew/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import upgrade from '../../../images/residential/upgrade.jpeg'
+import upgrade from '../../../images/residential/upgrade.jpeg';
+import upgradeSmall from '../../../images/residential/upgradeSmall.jpg';
 import './style.css';
 
 function UpgradeNew() {
 
     return(
         <div className='upgrade'>
-            <img src={upgrade} alt='Modern Neon Home' />
+            <img src={upgrade} alt='Modern Neon Home' className='sm-screen-hide' />
+            <img src={upgradeSmall} alt='' className='lg-screen-hide' />
             <div className='overlay'></div>
             <div className='text'>
                 <p>Transform your backyard into a</p>
