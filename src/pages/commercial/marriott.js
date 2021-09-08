a7e4d1462ec987ce5d022f1b888df083d2f1d5c9
Sarah Carter <carters@transblue.org>
2021-09-08 11:41:18 -0700

    adjusted margin of marriott img on sm screens and a.cta / button.cta padding

--- src/pages/commercial/marriott.js
+++ src/pages/commercial/marriott.js
@@ -15,7 +15,7 @@ function Marriott() {
                     <p className='section-p'>Asphalt maintenence is essential in commercial properties. Applying a seal coat overlay to the existing asphalt keeps the parking lot looking fresh and sharp.</p>
                     <p className='section-p'>When working with a worldwide hospitality client such as Marriott, it is crucial to be on time, on budget, and have minimal impact on guests.</p>
                     <p className='section-p'>The service was delivered with WorldClass efficiency and established Transblue as a reliable partner for Marriott's future needs.</p>
-                    <a className='cta' href='/locations'>FIND A LOCATION</a>
+                    <a className='cta' href='/locations'>LOCATIONS</a>
                     <ContactModal />
                 </div>
             </div>
