d37e594b5c9d4db5fa9c6998472584fd8511c66b
Sarah Carter <carters@transblue.org>
2021-09-09 10:29:52 -0700

    made landing core2 component mobile responsive

--- src/components/landing/core2/index.js
+++ src/components/landing/core2/index.js
@@ -8,39 +8,50 @@ function Core2() {
     return(
         <div className='core2-wrapper container-fluid d-lg-none'>
             <div className='row'>
+                <div className='col-12 text-center'>
+                    <h4 className='section-header'>
+                        TRANSBLUE CORE VALUES
+                    </h4>
+                    <p className='section-p'>
+                        {/* At Transblue, we understand the importance of having values within the company that bleed over */}
+                    Massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est
+                    </p>
+                </div>
+            </div>
+            <div className='row'>
                 <div className='col-lg-6'>
                     <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center pr-0'>
+                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
                             <img src={core1} alt='' />
                         </div>
-                        <div className='col-9 col-lg-10 my-auto'>
-                            <h4 className='section-header'>SEE IT THROUGH</h4>
+                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
+                            <h4 className='subheader'>SEE IT THROUGH</h4>
                             <p className='section-p'>
-                            Nunc pellentesque malesuada nunc in varius. Phasellus placerat magna at justo semper.
+                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
                             </p>
                         </div>
                     </div>
 
                     <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center pr-0'>
+                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
                             <img src={core3} alt='' />
                         </div>
-                        <div className='col-9 col-lg-10 my-auto'>
-                            <h4 className='section-header'>HAVE GRIT</h4>
+                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
+                            <h4 className='subheader'>HAVE GRIT</h4>
                             <p className='section-p'>
-                            Nunc pellentesque malesuada nunc in varius. Phasellus placerat magna at justo semper.
+                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
                             </p>
                         </div>
                     </div>
 
                     <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center pr-0'>
+                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
                             <img src={core2} alt='' />
                         </div>
-                        <div className='col-9 col-lg-10 my-auto'>
-                            <h4 className='section-header'>BE ENGAGED</h4>
+                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
+                            <h4 className='subheader'>BE ENGAGED</h4>
                             <p className='section-p'>
-                            Nunc pellentesque malesuada nunc in varius. Phasellus placerat magna at justo semper.
+                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
                             </p>
                         </div>
                     </div>
