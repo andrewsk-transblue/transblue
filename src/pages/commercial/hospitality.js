cecd25f43af2af63c25b03b2fcb63e3ba4168f84
Sarah Carter <carters@transblue.org>
2021-09-16 11:17:57 -0700

    deleted unused css for old hospitality wrapper on commercial page, and deleted old hospitality js

--- src/pages/commercial/hospitality.js
+++ src/pages/commercial/hospitality.js
@@ -26,17 +26,17 @@ function Hospitality() {
             <p className='section-p'>We're a one-stop shop for all your commercial construction needs.</p>
 
             <div className='row'>
-                <div className='col-12 col-lg-4'>
+                <div className='col-12 col-md-6 col-lg-4'>
                     <img src={turnkey} alt='' />
                     <h5>FLEXIBLE TURNKEY SOLUTION</h5>
                     <p className='section-p'>Our flexible turnkey solution allows you to focus on managing your business rather than managing contractors</p>
                 </div>
-                <div className='col-12 col-lg-4'>
+                <div className='col-12 col-md-6 col-lg-4'>
                     <img src={customers} alt='' />
                     <h5>CLIENT COLLABORATION</h5>
                     <p className='section-p'>We collaborate with clients to provide industry expertise that parallels the project vision, ensuring the project outcome is methodically designed and brought to completion with a World Class finish</p>
                 </div>
-                <div className='col-12 col-lg-4'>
+                <div className='col-12 col-md-6 col-lg-4'>
                     <img src={turnkey} alt='' />
                     <h5>INDUSTRY EXPERTS</h5>
                     <p className='section-p'>Transblue provides industry experts at each turn of the project to ensure that the delivery is as expectation is exceeded.</p>
@@ -44,41 +44,6 @@ function Hospitality() {
 
             </div>
         </div>
-        // <div className='hospitality-wrapper container-fluid'>
-        //     <div className='row'>
-        //         <div className='col-12 col-lg-6 order-lg-12'>
-        //             <div className='about-slideshow'>
-        //                 {index === 0 && <Fade opposite duration={1000} ><img src={images[index]} alt='commercial landscaping' /></Fade>}
-        //                 {index === 1 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-        //                 {index === 2 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-        //                 {index === 3 && <Fade opposite duration={1000}><img src={images[index]} alt='commercial landscaping' /></Fade>}
-        //             </div>
-        //         </div>
-        //         <div className='col-12 col-lg-6 hospitality-text'>
-        //             <h6>NO MATTER THE PROJECT,</h6>
-        //             <h4 className='section-header hospitality-header'>
-        //                 YOU CAN COUNT ON TRANSBLUE
-        //             </h4>
-        //             <p className='section-p mb-2'>We're a one-stop shop for all your commercial construction needs.</p>
-        //             <p className='hospitality-subheader'>
-        //                 <img src={check} className='check-icon' alt='' /> FLEXIBLE TURNKEY SOLUTION
-        //             </p>
-        //             <p className='section-p'>Allowing you to focus on managing your business rather than managing contractors</p>
-        //             <p className='hospitality-subheader'>
-        //                 <img src={check} className='check-icon' alt='' /> GREAT COMMUNICATION, MANAGEMENT, AND EXECUTION
-        //             </p>
-        //             <p className='section-p'>Transblue always aims to make your life easy</p>
-        //             <p className='hospitality-subheader'>
-        //                 <img src={check} className='check-icon' alt='' /> INDUSTRY EXPERTS
-        //             </p>
-        //             <p className='section-p'>Transblue provides industry experts at each turn of the project to ensure that the delivery is as expectation is exceeded. </p>
-        //             <p className='hospitality-subheader'>
-        //                 <img src={check} className='check-icon' alt='' /> CLIENT COLLABORATION
-        //             </p>
-        //             <p className='section-p'>We provide industry expertise that parallels the project vision, ensuring the project outcome is methodically designed and brought to completion with a World Class finish. </p>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
