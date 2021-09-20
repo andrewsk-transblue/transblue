5b30a2ba752ec02059a6f080676fa3df5def71c1
Sarah Carter <carters@transblue.org>
2021-09-20 16:58:17 -0700

    added text to franchise govt component

--- src/pages/franchise/govt.js
+++ src/pages/franchise/govt.js
@@ -7,7 +7,7 @@ function Govt(props) {
         <Section
             img={govtFence}
             title='GOVERNMENT SERVICES'
-            p1={`At ${props.location.name},`}
+            p1={`${props.location.name} provides a full range of construction services to Government Facilities, whether it be Federal Government, Military, Municipality, School Districts, Parks and Recreation, etc. Transblue is positioned to deliver on the projects you’re planning to improve your properties. With a track record of success, we understand the scope from paperwork to prevailing wage and construction to completion.`}
             p2=''
             p3=''
             contact={props.location}
