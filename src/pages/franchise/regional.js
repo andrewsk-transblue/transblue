ae3c3aac05bf6c75f8a8b1dd745b70c641d149a1
Sarah Carter <carters@transblue.org>
2021-09-23 11:46:03 -0700

    added more images to regional.js

--- src/pages/franchise/regional.js
+++ src/pages/franchise/regional.js
@@ -1,10 +1,17 @@
 import wcommercial from '../../images/franchise/wcommercial.jpg';
 import wmulti from '../../images/franchise/wmulti.jpg';
 import wev from '../../images/franchise/wev.jpg';
+import nwev from '../../images/government/evCropped.jpg';
+import nwcommercial from '../../images/franchise/commercial.jpg';
+import nwmulti from '../../images/franchise/multifamily.jpeg';
+import smulti from '../../images/franchise/smulti.jpg';
+
 
 const regional = {
     'west': [wcommercial, wmulti, wev],
-    'northwest': []
+    'northwest': [nwcommercial, nwmulti, nwev],
+    'southwest': [wcommercial, smulti, wev] ,
+    'midwest': []
 }
 
 export default regional;
