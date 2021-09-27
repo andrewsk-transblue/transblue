dd1406461d8c9a46478f559247b68f0ebedf33f1
Sarah Carter <carters@transblue.org>
2021-09-27 13:37:12 -0700

    added small images to rockstar component

--- src/components/residential/rockstar/index.js
+++ src/components/residential/rockstar/index.js
@@ -4,12 +4,39 @@ import monohan6 from '../../../images/residential/monohan6.jpg';
 import monohan6small from '../../../images/residential/monohan6small.jpg';
 import forward from '../../../images/residential/forward.png';
 import retainingdrops from '../../../images/residential/retainingdrops.jpg';
+import retainingdropssmall from '../../../images/residential/retainingdropssmall.jpg';
 import monohan4 from '../../../images/residential/monohan4.jpg';
+import monohan4small from '../../../images/residential/monohan4small.jpg';
 import monohan3 from '../../../images/residential/monohan3.jpg';
+import monohan3small from '../../../images/residential/monohan3small.jpg';
 import monohan from '../../../images/residential/monohan.jpg';
+import monohansmall from '../../../images/residential/monohansmall.jpg';
 import './style.css';
 
-const images = [monohan6, retainingdrops, monohan4, monohan3, monohan]
+//const images = [monohan6, retainingdrops, monohan4, monohan3, monohan]
+
+const images = [
+    {
+        img: monohan6,
+        imgSmall: monohan6small
+    },
+    {
+        img: retainingdrops,
+        imgSmall: retainingdropssmall
+    },
+    {
+        img: monohan4,
+        imgSmall: monohan4small
+    },
+    {
+        img: monohan3,
+        imgSmall: monohan3small
+    },
+    {
+        img: monohan,
+        imgSmall: monohansmall
+    }
+]
 
 function Rockstar() {
     const [index, setIndex] = useState(0);
@@ -25,7 +52,9 @@ function Rockstar() {
     return(
         <div className='rockstar-wrapper container-fluid px-0'>
             {/* <img src={images[index]} alt='' /> */}
-            <img src={monohan6small} alt='' />
+            <img src={images[index].imgSmall} 
+                srcSet={`${images[index].imgSmall} 600w, ${images[index].img} 1400w`} sizes='(max-width: 600px) 600px, 1400px'
+             />
             <div className='text row mx-0 px-0'>
                 <div className='text-center col-2 col-sm-1'><img id='drop' src={drop} alt='' /></div>
                     <div className='col-8 col-sm-9 my-auto'>
