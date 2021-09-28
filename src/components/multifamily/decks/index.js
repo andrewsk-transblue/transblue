ef70d4f697057f0b2518bd4d49ce43565d354b9d
Sarah Carter <carters@transblue.org>
2021-09-28 15:12:47 -0700

    added hospitality section to multifamily page

--- src/components/multifamily/decks/index.js
+++ src/components/multifamily/decks/index.js
@@ -8,7 +8,7 @@ function Decks() {
         <div className='row'>
             <div className='col-lg-5 col-md-12 order-lg-12 left-text'>
                 <h4 className='section-header'>INCREASE SECURITY &amp; PRIVACY</h4>
-                <p className='section-p'>Transblue understands how to build and rebuild decks on your community properties. We understand that these decks are connected directly to your living space, and that their resurfacing or reconstruction needs to be done quickly and efficiently with a high quality to minimize impact to your life. That is why Transblue is truly the right partner for your decking needs. </p>
+                <p className='section-p'>At Transblue, we understand decks on your community properties. We understand that these decks are connected directly to your living space, and that their resurfacing or reconstruction needs to be done quickly and efficiently with high quality to minimize impact to your life. That is why Transblue is truly the right partner for your decking needs. </p>
                 <p className='section-p'>Fencing is key to privacy and needs to be replaced every 15-20 years. Whether your increasing property privacy or just replacing worn out fencing, Transblue is the right choice for you! </p>
             </div>
             <div className='col-lg-7 col-md-6'>
--- src/pages/multifamily/index.js
+++ src/pages/multifamily/index.js
@@ -12,6 +12,7 @@ import ServiceList from '../../components/serviceList';
 import Snow from '../../components/snow';
 import ContactModal from '../../components/contactModal';
 import ThreeCols from '../../components/threeCol';
+import Hospitality from '../commercial/hospitality';
 import Footer from '../../components/footer';
 import header2 from '../../images/multifamily/header2.jpg';
 import rooftop from '../../images/multifamily/rooftop.jpeg';
@@ -66,6 +67,7 @@ function Multifamily() {
                         </div>
                     </div>
                 </div>
+                <Hospitality />
                 {/* <div className='benefits-banner'>
                     <p>INCREASE YOUR PROPERTY VALUE</p>
                     <p>with our World Class services that enhance the beauty of every space </p>
@@ -75,7 +77,7 @@ function Multifamily() {
                     subtitle='Refreshed buildings keep property values high. Transblue is 100% committed to delivering World Class services that enhance the beauty of each and every space.'
                     card1={{
                         img: rooftop,
-                        title: 'A BRAND NEW ROOF',
+                        title: 'RETHINK YOUR ROOF',
                         body: 'Transblue provides roofing services to communities looking to replace existing roofing structures or install new roofing systems. This includes systems such as flat PVC and TPO, metals, asphalt shingles, green roofs, tiles, torch down, and more'
                     }}
                     card2={{
@@ -148,7 +150,7 @@ function Multifamily() {
                 <div className='container-fluid camp'>
                     <div className='row'>
                         <div className='col-lg-5 order-lg-12 left-text'>
-                            <h4 className='section-header'>TRANSBLUE SUPPORTS KIDS CAMPS</h4>
+                            <h4 className='section-header'>CREATING A SOLID FOUNDATION FOR YOUTH</h4>
                             {/* <div className='section-p mb-3'>Summer is a great time for camps. Camps are transformational in the lives of children and teens. Solid Rock Mission, one of our mBridge partners, hosts camps all summer long for thousands of kids and teens (1782 kids in June).</div>
                             <div className='section-p mb-3'>This month we have had back to back to back camps. We organized camps in villages and small towns. Over 100 kids showed up to each camp with 1782 attending in June. These kids come from various families and backgrounds with all sorts of hurts and broken stories. Our goal is to always and foremost show them God's love.</div>
                             <div className='section-p pb-4'>Most of the kids that attended these camps over the course of this month come from broken families. Many of these families lost their loved ones while others had parents that simply left their family. At camp, it is a great opportunity for us to love them and most of all show them God's love.</div> */}
@@ -157,7 +159,7 @@ function Multifamily() {
                             </div>
                             {/* <div className='section-p mb-3'>Summer is a great time for camps. Camps are transformational in the lives of children and teens.</div>
                             <div className='section-p mb-3'></div> */}
-                            <div className='section-p mb-3'>Last year, Transblue helped Solid Rock Mission send over 4000 children and youth from Southern and Eastern Ukraine to summer camps with over 1000 committing their lives to Christ. These camps were a great opportunity to impact these children with hope and love in a trying time, and more importantly show them God's love.</div>
+                            <div className='section-p mb-3'>Last year, we completed the Ascent roofing project in Seattle, WA. We partnered with Solid Rock Mission and used the profits from the Ascent project to send over 4000 children and youth from Southern and Eastern Ukraine to summer camps. Over 1000 of these children committed their lives to Christ. These camps were a great opportunity to impact these children with hope and love in a trying time, and more importantly show them God's love.</div>
                         </div>
                         <div className='col-lg-7'>
                             <CampImages />
