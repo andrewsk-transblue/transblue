c07a56c9e4f33ab4c6d02f7a778b67b2eb16e96c
Sarah Carter <carters@transblue.org>
2021-09-30 13:54:32 -0700

    added spokane zipcodes, cities, and geojson to easybase

--- src/pages/locations/index.js
+++ src/pages/locations/index.js
@@ -5,6 +5,7 @@ import Footer from '../../components/footer/index';
 import MapContainer from './mapcontainer';
 import ListView from './listView';
 import axios from 'axios';
+import zipcodes from 'zipcodes';
 
 //import bounds from './bounds';
 import './style.css';
@@ -14,9 +15,48 @@ import './style.css';
 function Locations(props) {
         //console.log(typeof JSON.parse(props.locations[0].zipcodelist.toString()))
         //let zipcodes = JSON.parse(props.locations[12].zipcodelist);
-        let zipcodes = []
-        let str = zipcodes.toString()
-        console.log(str)
+        let zipcodeArray = [
+            83854,
+            99001,
+            99003,
+            99005,
+            99011,
+            99016,
+            99019,
+            99021,
+            99022,
+            99025,
+            99027,
+            99037,
+            99201,
+            99202,
+            99203,
+            99204,
+            99205,
+            99206,
+            99207,
+            99208,
+            99212,
+            99216,
+            99217,
+            99218,
+            99223,
+            99224,
+            83877,
+            99209,
+            99210,
+            99211,
+            99213,
+            99214,
+            99215,
+            99219,
+            99220,
+            99228,
+            99252,
+            99256
+            ]
+        let str = zipcodeArray.toString()
+        //console.log(str)
 
         const options = {
             method: 'GET',
