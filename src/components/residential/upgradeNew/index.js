460fd1dd0c4252791879ff2e0990d2855673496a
Sarah Carter <carters@transblue.org>
2021-10-08 11:51:59 -0700

    made upgrade image background image

--- src/components/residential/upgradeNew/index.js
+++ src/components/residential/upgradeNew/index.js
@@ -6,7 +6,7 @@ import './style.css';
 function UpgradeNew() {
     return(
         <div className='upgrade'>
-            <img src={upgradeSmall} alt='Modern Neon Home' srcSet={`${upgradeSmall} 576w, ${upgrade} 1400w`} sizes='(max-width: 576px) 576px, 1400px' />
+            {/* <img src={upgradeSmall} alt='Modern Neon Home' srcSet={`${upgradeSmall} 576w, ${upgrade} 1400w`} sizes='(max-width: 576px) 576px, 1400px' /> */}
             <div className='overlay'></div>
             <div className='text'>
                 <p>Transform your backyard into a</p>
