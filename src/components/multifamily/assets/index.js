b4644fdbebaee49bacf4e8f56170bcc265ee6d55
Sarah Carter <carters@transblue.org>
2021-10-11 09:18:03 -0700

    made multifamily assets component mobile responsive

--- src/components/multifamily/assets/index.js
+++ src/components/multifamily/assets/index.js
@@ -11,14 +11,14 @@ function Assets() {
     return(
         <Fragment>
             <div className='assets-wrapper container-fluid w-100 px-0'>
-                <img src={assetsSmall} alt='Multifamily Residence' srcSet={`${assetsSmall} 576w, ${assets} 2000w`} />
+                {/* <img src={assetsSmall} alt='Multifamily Residence' srcSet={`${assetsSmall} 576w, ${assets} 2000w`} /> */}
                 <div className='overlay'></div>
                 <div className='text row mx-0'>
-                    <div className='col-lg-9 pl-4'>
+                    <div className='col-lg-9 col-10 pl-4'>
                         <h3>PROTECT YOUR ASSETS</h3>
                         <p>Investing in your property is key to keeping up property values. Take care of the common elements and work with Transblue to deliver a World Class finish to your community's capital projects.</p>
                     </div>
-                    <div className='col-lg-3 my-auto text-center'>
+                    <div className='col-lg-3 col-2 my-auto text-center'>
                         <div onClick={executeScroll}><img className='next-section' src={downarrow} alt='Next Section' /></div>
                     </div>
                 </div>
