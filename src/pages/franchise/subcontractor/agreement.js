fad9f6b1390e239bbda50cf98517cdc0f72bcea1
Sarah Carter <carters@transblue.org>
2021-10-11 13:17:27 -0700

    dangerously set innerhtml of master subcontractor agreement

--- src/pages/franchise/subcontractor/agreement.js
+++ src/pages/franchise/subcontractor/agreement.js
@@ -3,7 +3,8 @@ import './style.css';
 
 function Agreement(props) {
     return(
-        <p>{props.location.subagreement}</p>
+        // <p>{props.location.subagreement}</p>
+        <p className='agreement-wrapper' dangerouslySetInnerHTML={{__html: props.location.subagreement}}></p>
     )
 }
 
