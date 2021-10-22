0fdf8ffe54fda9a23dd4a6be24b87d1c4c9a2539
Sarah Carter <carters@transblue.org>
2021-10-22 10:49:11 -0700

    dont display captcha until all fields in form are completed

--- src/pages/contact/form.js
+++ src/pages/contact/form.js
@@ -57,28 +57,28 @@ function Form() {
             <p className='title'>Feel free to leave us a message below</p>
             <div className='row'>
                 <div className='col-sm-6 my-1 input-left'>
-                    <input placeholder='First Name' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+                    <input placeholder='First Name' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} required></input>
                 </div>
                 <div className='col-sm-6 my-1 input-right'>
-                    <input placeholder='Last Name' id='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)}></input>
+                    <input placeholder='Last Name' id='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)} required></input>
                 </div>
             </div>
             <div className='row'>
                 <div className='col-sm-6 my-2 input-left'>
-                    <input placeholder='Email' id='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                    <input placeholder='Email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required></input>
                 </div>
                 <div className='col-sm-6 my-2 input-right'>
-                    <input placeholder='Phone Number' id='phone' value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+                    <input placeholder='Phone Number' id='phone' value={phone} onChange={(e) => setPhone(e.target.value)} required></input>
                 </div>
             </div>
             <div className='row'>
                 <div className='col-sm-12 px-0 mt-2'>
-                    <textarea placeholder='What can we help you with?' id='message' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                    <textarea placeholder='What can we help you with?' id='message' value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
                 </div>
             </div>
             <div className='row'>
                 {!disabled && <button onClick={handleSubmit} data-dismiss='modal' >SEND MESSAGE</button>}
-                {disabled && <Captcha onChange={() => setDisabled(false)} />}
+                {(disabled && formCompleted) && <Captcha onChange={() => setDisabled(false)} />}
             </div>
 
         </form>
