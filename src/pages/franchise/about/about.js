8b0dbf96582d602da63e13ea3e648e88bc16a398
Sarah Carter <carters@transblue.org>
2021-10-22 11:25:01 -0700

    changed margin of franchise about to right margin on email instead of left margin on phone

--- src/pages/franchise/about/about.js
+++ src/pages/franchise/about/about.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import './style.css';
 
-// function About(props) {
-//     return(
-//         <fieldset>
-//             <legend>A landscaping company you can trust</legend> 
-//             <p className='text-secondary'>Donec non neque imperdiet, pretium ex ut, tristique mi. Suspendisse sed tellus a ipsum viverra pharetra pharetra nec quam. Duis quis consequat justo, vel vulputate nulla. Vivamus sit amet viverra risus. Pellentesque maximus ac mi in efficitur. Sed vitae congue ex. Ut in ultricies elit. Phasellus et tincidunt lacus, aliquam lacinia ex. Nunc accumsan aliquam blandit.</p>
-//             <span>
-//                 <div className='franchise-phone'><i className="fas fa-phone"></i>  {props.phone}</div>
-//                 <div className='franchise-email'><i className="fas fa-envelope"></i> {props.email}</div>
-//             </span>
-        
-//         </fieldset>
-//     )
-// }
-
 function About(props) {
     //console.log(props)
 
