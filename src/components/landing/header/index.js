6caede5a3b693873c71f7011d016849ca5f03ae8
Sarah Carter <carters@transblue.org>
2021-10-22 15:19:32 -0700

    change landing header text using javascript

--- src/components/landing/header/index.js
+++ src/components/landing/header/index.js
@@ -1,20 +1,42 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Franchise from '../../../pages/franchise/franchise';
 import FranchiseNav from '../../franchiseNav';
 
 function Header() {
-        return(
-            <Fragment>
-                <FranchiseNav />
-                <div className='header'>
-                <div id='header_slide'>
-                    <div className='landing-header'>
-                    </div>
-                    <div className='overlay'></div>
-                </div>
+    const [text, setText] = useState('LUXURY EXTERIOR DESIGN');
+
+    let index = 0;
+    const textArray = [
+        'LUXURY EXTERIOR DESIGN',
+        'A GREENER WORLD',
+        'LUXURY EXTERIOR DESIGN',
+        'QUALITY ASPHALT INSTALLATION',
+        'LUXURY EXTERIOR DESIGN',
+        'BEAUTIFUL COMMUNITY LIVING',
+        'LUXURY EXTERIOR DESIGN',
+        'RELIABLE SNOW REMOVAL'
+    ]
+
+    useEffect(() => {
+        setInterval(() => {
+            if(index < textArray.length - 1) index += 1
+            else index = 0
+            console.log(index)
+            setText(textArray[index])
+        }, 12000)
+    }, [])
+
+    return(
+        <Fragment>
+            <FranchiseNav />
+            <div className='header'>
+            <div id='header_slide'>
+                <div className='landing-header'>{text}</div>
+                <div className='overlay'></div>
             </div>
-            </Fragment>
-        )
+        </div>
+        </Fragment>
+    )
 }
 
 export default Header;
