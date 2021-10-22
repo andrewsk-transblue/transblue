95dcbe00d650a156177f4b3cd38fcaaff91dc5d2
Sarah Carter <carters@transblue.org>
2021-10-22 10:41:21 -0700

    zoom in on franchise location map

--- src/pages/franchise/map.js
+++ src/pages/franchise/map.js
@@ -14,7 +14,7 @@ function Map(props) {
     }, [props.userLocation])
 
     return(
-        <MapContainer center={position} zoom={9} scrollWheelZoom={true} id='mapid'>
+        <MapContainer center={position} zoom={10} scrollWheelZoom={true} id='mapid'>
             <TileLayer
                 attribution='Transblue'
                 url='https://api.mapbox.com/styles/v1/sarahmariecarter/ckt259o46010e18njaz84sofg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FyYWhtYXJpZWNhcnRlciIsImEiOiJja3EyaXBub2UwcDVnMm9xYjY1M282MWtsIn0.M45SX1Os1C-H17ssCoAL8g'
