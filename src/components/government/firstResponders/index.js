bd0b3e81deae2645c373db9c0b1f96f2533e6f92
Sarah Carter <carters@transblue.org>
2021-11-08 11:46:12 -0800

    added link to fire department asphalt youtube video to government gives back component

--- src/components/government/firstResponders/index.js
+++ src/components/government/firstResponders/index.js
@@ -13,7 +13,8 @@ function FirstResponders() {
                     <h4 className='section-header'>TRANSBLUE AND FIRST RESPONDERS</h4>
                     <p className='section-p'>Hope Unlimited, one of our mBridge partners, provides critical support to both our first responders and the families of Snohomish County that are in crisis. They provide comfort, support and training during crisis events in our county.</p>
                     <p className='section-p'>Hope Unlimited serves 13 different first responder agencies and 1 hospital each of the following counties: Snohomish, Island, and South Skagit Counties. They had 363 calls for service in 2020 – this means that a chaplain was requested by the fire, police or hospital for assistance in a critical incident.</p>
-                    <p className='section-p'>They are adding a new resiliency training programs for first responders to help them cope with challenges of their jobs. team members.</p>
+                    {/* <p className='section-p'>They are adding a new resiliency training programs for first responders to help them cope with challenges of their jobs. team members.</p> */}
+                    <a href='https://www.youtube.com/watch?v=OYaija3jk-8' className='cta' target='_blank' rel='noreferrer'>LEARN MORE</a>
                 </div>
             </div>
         </div>
