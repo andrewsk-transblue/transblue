2196563066845f882b7fb1ecf632a0b430b9946a
Sarah Carter <carters@transblue.org>
2021-11-15 09:37:10 -0800

    added about section to careers page

---
+++ src/pages/careers/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../images/careers/logo.png';
+import './style.css';
+
+function About() {
+    return(
+        <div className='about-wrapper'>
+            <img src={logo} alt='Transblue Logo' />
+            <h2>JOIN OUR TRANSBLUE FAMILY</h2>
+            <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus est in quam sodales, at imperdiet risus bibendum. Nulla varius, leo sit amet vestibulum ornare, magna lorem eleifend tortor, in tincidunt risus diam id nunc. Suspendisse varius arcu non diam lacinia, sed pharetra mauris dapibus.</p>
+        </div>
+    )
+}
+
+export default About;
--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/header';
 import Navbar from '../../components/navbar';
+import About from './about';
 import './style.css';
 
 function Careers() {
@@ -8,6 +9,7 @@ function Careers() {
         <Fragment>
             <Header title='TRANSBLUE CAREERS' />
             <Navbar />
+            <About />
         </Fragment>
     )
 }
