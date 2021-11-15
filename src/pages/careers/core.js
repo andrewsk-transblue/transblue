4d9fbeec0fcc53722c70738fd9f5a714c47951ba
Sarah Carter <carters@transblue.org>
2021-11-15 11:43:00 -0800

    added core values section to career component. Added fade animation to core value images

--- src/pages/careers/core.js
+++ src/pages/careers/core.js
@@ -1,56 +1,60 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade'
+import through from '../../images/careers/through.png';
+import engaged from '../../images/careers/engaged.png';
+import nimble from '../../images/careers/nimble.png';
+import grit from '../../images/careers/grit.png';
+import help from '../../images/careers/help.png';
+import eachother from '../../images/careers/eachother.png';
 import './style.css';
 
 function Core() {
     return(
-        <div className='core-wrapper container-fluid'>
+        <div className='core-wrapper'>
             <div className='row'>
-                <div className='col-12 text-center'>
-                    <h4 className='section-header'>
-                        TRANSBLUE CORE VALUES
-                    </h4>
-                    <p className='section-p'>
-                        {/* At Transblue, we understand the importance of having values within the company that bleed over */}
-                    Massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est
-                    </p>
+                <div className='col-lg-5 pr-5'>
+                    <h2>WHO ARE WE?</h2>
+                    <p className='section-p'>At Transblue, we prioritize the well being of our employees, our customers, and the community. We have curated a list of our core values that we live by, both in and out of the workplace. </p>
+                    <p className='section-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis est ex. Sed enim orci, porttitor nec volutpat a, fringilla eu dui. Cras elit ex.</p>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='col-lg-6'>
-                    <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
-                            {/* <img src={core1} alt='' /> */}
+                <div className='col-lg-1'></div>
+                <div className='col-lg-6 my-auto'>
+                    <div className='row ml-5'>
+                        <div className='col-4' style={{marginLeft: '-16%'}}>
+                            <Fade duration={1200}>
+                                <img src={engaged} alt='' />
+                            </Fade>
                         </div>
-                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
-                            <h4 className='subheader'>SEE IT THROUGH</h4>
-                            <p className='section-p'>
-                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
-                            </p>
+                        <div className='col-4'>
+                            <Fade duration={1200} delay={400}>
+                                <img src={nimble} alt='' />
+                            </Fade>
                         </div>
-                    </div>
 
-                    <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
-                            {/* <img src={core3} alt='' /> */}
+                        <div className='col-4'>
+                            <Fade duration={1200} delay={800}>
+                                <img src={grit} alt='' />
+                            </Fade>
                         </div>
-                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
-                            <h4 className='subheader'>HAVE GRIT</h4>
-                            <p className='section-p'>
-                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
-                            </p>
-                        </div>
-                    </div>
 
-                    <div className='row'>
-                        <div className='col-3 col-lg-2 my-auto text-center px-0'>
-                            {/* <img src={core2} alt='' /> */}
+                        <div className='col-4'  >
+                            <Fade duration={1200} delay={1200}>
+                                <img src={help} alt=''/>
+                            </Fade>
                         </div>
-                        <div className='col-9 col-md-8 col-lg-10 my-auto'>
-                            <h4 className='subheader'>BE ENGAGED</h4>
-                            <p className='section-p'>
-                            Nunc pellent malesuada nunc in varius. Phasellus placerat magna at justo semper.
-                            </p>
+
+                        <div className='col-4'>
+                            <Fade duration={1200} delay={1600}>
+                                <img src={eachother} alt=''/>
+                            </Fade>
                         </div>
+
+                        <div className='col-4'>
+                            <Fade duration={1200} delay={2500}>
+                            <img src={through} alt=''/>
+                            </Fade>
+                        </div>
+
                     </div>
                 </div>
             </div>
