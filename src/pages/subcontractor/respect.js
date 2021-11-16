e7265210d603b48eef87dcbc889ce21924ff5187
Sarah Carter <carters@transblue.org>
2021-11-16 11:06:00 -0800

    adjusted styles and width of subcontractor respect component

--- src/pages/subcontractor/respect.js
+++ src/pages/subcontractor/respect.js
@@ -6,8 +6,8 @@ function Respect() {
     return(
         <div className='respect-wrapper'>
             <div className='row'>
-                <div className='col-lg-5 my-auto'>
-                    <h1>WE WERE BUILT<br />ON RESPECT.</h1>
+                <div className='col-lg-5 my-auto pl-0'>
+                    <h2>WE WERE BUILT<br />ON RESPECT.</h2>
                     <p className='section-p'>At Transblue, we believe respect is the foundation of any successful business.</p>
                     <button className='cta ml-0 mt-3'>APPLY NOW</button>
                 </div>
