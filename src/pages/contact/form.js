e0d7e197f77f9813317cef4e5230d19a5aba945c
Sarah Carter <carters@transblue.org>
2022-04-05 14:36:29 -0700

    added submit button to contact page form. Disabled it if not all fields are filled out.

--- src/pages/contact/form.js
+++ src/pages/contact/form.js
@@ -76,7 +76,7 @@ function Form() {
                 </div>
             </div>
             <div className='row'>
-                {!disabled && <button onClick={handleSubmit} data-bs-dismiss='modal' >SEND MESSAGE</button>}
+                <button disabled={disabled} className={disabled ? 'disabled' : ''} onClick={handleSubmit} data-bs-dismiss='modal' >SEND MESSAGE</button>
                 {(disabled && formCompleted) && <Captcha onChange={() => setDisabled(false)} />}
             </div>
 
