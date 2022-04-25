94c1e428976cb2baa18fdc34ba107f677aaf1f32
Sarah Carter <carters@transblue.org>
2022-04-25 16:37:31 -0700

    adjusted styles of caseStudy component

--- src/pages/commercial/caseStudy.js
+++ src/pages/commercial/caseStudy.js
@@ -6,7 +6,7 @@ function CaseStudy() {
     return(
         <div className='case-study'>
             <div className='case-study-content'>
-                <h3 className=''>TRANSBLUE CASE STUDY</h3>
+                <h3 className=''>TRANSBLUE CASE STUDIES</h3>
                 <p>arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus</p>
                 <button type="button"data-bs-toggle="modal" data-bs-target="#caseStudyModal">
                     DOWNLOAD A CASE STUDY
