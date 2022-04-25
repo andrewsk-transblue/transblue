42ca5c622505d3b879d9686e071774352b11f426
Sarah Carter <carters@transblue.org>
2022-04-25 15:04:14 -0700

    added form validation and send email from caseStudyModal form

--- src/components/caseStudyModal/caseStudyModal.js
+++ src/components/caseStudyModal/caseStudyModal.js
@@ -1,15 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import{ init } from 'emailjs-com';
+import * as emailjs from 'emailjs-com'
+init("user_iLZ3jXyTzXi5zQFlgf5DG");
 
 function CaseStudyModal() {
-
-    const [caseStudy, setCaseStudy] = useState('');
-
     const [formValues, setFormValues] = useState({
         name: '',
         company: '',
         email: '',
-        phone: ''
+        phone: '',
+        caseStudy: 'casestudy1'
     })
+
+    const [submitted, setSubmitted] = useState(false);
+
+    function submit(e) {
+
+        setSubmitted(true);
+
+        if(formValues.name.length > 0 && formValues.email.length > 0 && formValues.phone.length > 0) {
+            // send form values via email        
+            emailjs.send(
+                'service_gekurtf',
+                'template_y21szg9', //CONTACT TEMPLATE
+                    formValues,
+                    process.env.REACT_APP_REACTJS_USER
+                )
+            
+            // close modal
+            window.$('#caseStudyModal').modal('hide');
+
+            // reset form values
+            setFormValues({
+                name: '',
+                company: '',
+                email: '',
+                phone: '',
+                caseStudy: ''
+            })
+        }
+        
+        else {
+
+        }
+    }
 
 
     return(
@@ -21,38 +56,51 @@ function CaseStudyModal() {
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        Select which case study you'd like to download.
+                        Select which case study you'd like to download: *
 
                         <div className="form-check mt-2">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="casestudy1" onChange={(e) => setCaseStudy(e.target.id)} checked={caseStudy === 'casestudy1'} />
+                            <input 
+                                className="form-check-input" 
+                                type="radio"
+                                 name="flexRadioDefault" 
+                                 id="casestudy1" 
+                                 onChange={(e) => setFormValues({...formValues, caseStudy: 'casestudy1'})}
+                                 checked={formValues.caseStudy === 'casestudy1'} />
                             <label className="form-check-label" for="flexRadioDefault1">
                                 Case Study 1
                             </label>
-                            </div>
-                            <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="casestudy2" checked={caseStudy === 'casestudy2'} onChange={(e) => setCaseStudy(e.target.id)} checked={caseStudy === 'casestudy2'} />
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="casestudy2" checked={formValues.caseStudy === 'casestudy2'} onChange={(e) => setFormValues({...formValues, caseStudy: 'casestudy2'})} />
                             <label className="form-check-label" for="flexRadioDefault2">
                                 Case Study 2
                             </label>
                         </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="casestudy3" checked={formValues.caseStudy === 'casestudy3'} onChange={(e) => setFormValues({...formValues, caseStudy: 'casestudy3'})} />
+                            <label className="form-check-label" for="flexRadioDefault2">
+                                Case Study 3
+                            </label>
+                        </div>
                     </div>
                     <div className="modal-body">
-                        <form>
-                            <label htmlFor="caseStudyName" className="form-label">Name:</label>
-                            <input className="w-100 mb-2 form-control" value={formValues.name} id='caseStudyName' onChange={(e) => setFormValues({...formValues, name: e.target.value})} />
+                        <form className="needs-validation" noValidate>
                             <label htmlFor="caseStudyCompany" className="form-label">Company:</label>
-                            <input className="w-100 mb-2 form-control" value={formValues.company} id='caseStudyCompany' onChange={(e) => setFormValues({...formValues, company: e.target.value})}  />
+                            <input className="w-100 mb-2 form-control" value={formValues.company} id='caseStudyCompany' onChange={(e) => setFormValues({...formValues, company: e.target.value})} />
 
-                            <label htmlFor="caseStudyPhone" className="form-label">Phone:</label>
-                            <input className="w-100 mb-2 form-control" value={formValues.phone} id="caseStudyPhone" onChange={(e) => setFormValues({...formValues, phone: e.target.value})}  />
+                            <label htmlFor="caseStudyName" className="form-label">Name: *</label>
+                            <input className={formValues.name.length === 0 && submitted ? "w-100 mb-2 form-control is-invalid" : "w-100 mb-2 form-control"} value={formValues.name} id='caseStudyName' onChange={(e) => setFormValues({...formValues, name: e.target.value})} required />
 
-                            <label htmlFor="caseStudyEmail" className="form-label">Email:</label>
-                            <input className="w-100 mb-2 form-control" type='email' value={formValues.email} id="caseStudyEmail" onChange={(e) => setFormValues({...formValues, email: e.target.value})}  />
+                            <label htmlFor="caseStudyPhone" className="form-label">Phone: *</label>
+                            <input className="w-100 mb-2 form-control" value={formValues.phone} id="caseStudyPhone" onChange={(e) => setFormValues({...formValues, phone: e.target.value})} required className={formValues.phone.length === 0 && submitted ? "w-100 mb-2 form-control is-invalid" : "w-100 mb-2 form-control"}  />
+
+                            <label htmlFor="caseStudyEmail" className="form-label">Email: *</label>
+                            <input className="w-100 mb-2 form-control" type='email' value={formValues.email}id="caseStudyEmail" onChange={(e) => setFormValues({...formValues, email: e.target.value})} required className={formValues.email.length === 0 && submitted ? "w-100 mb-2 form-control is-invalid" : "w-100 mb-2 form-control"}  />
                         </form>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button onClick={() => console.log(caseStudy)} type="button" className="btn btn-primary">Submit</button>
+                        <button onClick={submit} type="submit" className="btn btn-primary">Submit</button>
                     </div>
                 </div>
             </div>
--- src/pages/commercial/caseStudy.js
+++ src/pages/commercial/caseStudy.js
@@ -5,9 +5,13 @@ function CaseStudy() {
 
     return(
         <div className='case-study'>
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#caseStudyModal">
-                Download a Case Study
-            </button>
+            <div className='case-study-content'>
+                <h3 className=''>TRANSBLUE CASE STUDY</h3>
+                <p>arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus</p>
+                <button type="button"data-bs-toggle="modal" data-bs-target="#caseStudyModal">
+                    DOWNLOAD A CASE STUDY
+                </button>
+            </div>
         </div>
     )
 }
