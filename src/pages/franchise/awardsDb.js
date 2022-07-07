f128af77febfda9e330fc6296c0c7c42302c6741
Sarah Carter <carters@transblue.org>
2022-07-07 15:01:56 -0700

    added awards for monroe office

--- src/pages/franchise/awardsDb.js
+++ src/pages/franchise/awardsDb.js
@@ -7,10 +7,14 @@ import homeadvisor from '../../images/awards/homeadvisor.png';
 import bbb from '../../images/awards/bbb.png';
 import disabled from '../../images/awards/disabled.jpeg';
 import ussbchamberlogo from '../../images/awards/ussbchamberlogo.jpg';
+import bbb2 from '../../images/awards/bbb2.png';
+import sima from '../../images/awards/sima.jpg';
+import wmbe from '../../images/wmbe.jpg';
 
 let awardsDb = {
     'spokane': [asm,  disabled, cai, veteran, westplains, homeadvisor, ussbchamberlogo],
-    'norfolk': [torch, homeadvisor, bbb]
+    'norfolk': [torch, homeadvisor, bbb],
+    'monroe': [torch, bbb2, sima, cai, wmbe]
 }
 
 export default awardsDb;
