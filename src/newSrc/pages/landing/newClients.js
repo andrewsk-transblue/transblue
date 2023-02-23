644ed95f1298e0b604ad15220efef68ddd4c9814
Sarah Carter <carters@transblue.org>
2023-02-23 14:34:52 -0800

    removed lazy loading from newClients images

--- src/newSrc/pages/landing/newClients.js
+++ src/newSrc/pages/landing/newClients.js
@@ -4,17 +4,22 @@ import multifamily from '../../images/multifamily.jpg';
 import government from '../../images/government.jpg';
 import grainger from '../../images/grainger2.jpg';
 import residential from '../../images/residential.jpeg';
+import multifamilyPreview from '../../images/previews/multifamily-tiny.jpg';
+import governmentPreview from '../../images/previews/government-tiny.jpg';
+import graingerPreview from '../../images/previews/grainger2-tiny.jpg';
+import residentialPreview from '../../images/previews/residential-tiny.jpg';
 import './landing.css';
 import { useState } from "react";
 import { useEffect } from "react";
+import ProgressiveImage from 'react-progressive-image';
 
 const ImageComp = (props) => {
     const { title, description, href, src, alt } = props;
     return (
         <Box sx={{width: '100%', height: '100%'}}>
             {/* <img src={src} alt={alt} syle={{position: 'absolute', top: 0, left: 0, width: '100%'}} /> */}
-            {/* <div className="client-overlay"/> */}
-            {/* <div className="service-description"> */}
+            <div className="client-overlay"/>
+            <div className="service-description">
                 <Typography sx={{color: '#0b56a4', pt: '40px', pb: 2, fontSize: { xl: '2vw' }}} variant='h2'>{title}</Typography>
                 {/* <Typography sx={{color: 'white', px: '40px', pb: 2}} variant='body2'>{description}</Typography> */}
                 <Button
@@ -25,7 +30,7 @@ const ImageComp = (props) => {
                 >
                     learn more
                 </Button>
-            {/* </div> */}
+            </div>
             <Typography variant='h2' sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', fontSize: {
                 xl: '2vw'
             }}}>{title}</Typography>
@@ -82,9 +87,13 @@ function NewClients() {
                                 src={`${grainger}?w=164&h=164&fit=crop&auto=format`}
                                 srcSet={`${grainger}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                 alt='Commercial Clients'
-                                loading="lazy"
+                                //loading="lazy"
                             />
                             
+                            {/* <ProgressiveImage src={grainger} placeholder={graingerPreview}>
+                                {src => <img src={src} alt="an image" />}
+                            </ProgressiveImage> */}
+
                             <ImageComp 
                                 title='COMMERCIAL'
                                 description='Our teams are focused on making your life simple and reducing the burden that comes with the capital projects required to keep your business looking its best. Our experts offer a full range of services from roofing, siding, asphalt, concrete, decking, fencing, to EV chargers and Solar installs, to Snow & Ice management and everything in between.'
@@ -96,7 +105,7 @@ function NewClients() {
                                 src={`${multifamily}?w=164&h=164&fit=crop&auto=format`}
                                 srcSet={`${multifamily}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                 alt='Multifamily new parking lot striping project'
-                                loading="lazy"
+                                //loading="lazy"
                             />
                             <ImageComp                            
                                 title='MULTIFAMILY'
@@ -109,13 +118,13 @@ function NewClients() {
                                 src={`${government}?w=164&h=164&fit=crop&auto=format`}
                                 srcSet={`${government}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                 alt='Government clients'
-                                loading="lazy"
+                                //loading="lazy"
                             />
                             <ImageComp 
                                 src={government}
                                 title='GOVERNMENT'
                                 description='Transblue provides a full range of construction services to Government Facilities, including Federal Government, Military, Municipality, School Districts, etc. With a track record of success, we understand the scope from paperwork to prevailing wage and construction to completion.'
-                                href='/government'
+                                //href='/government'
                             />
                         </ImageListItem>
                         <ImageListItem>
@@ -123,7 +132,7 @@ function NewClients() {
                                 src={`${residential}?w=164&h=164&fit=crop&auto=format`}
                                 srcSet={`${residential}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                 alt='Residential Clients'
-                                loading="lazy"
+                                //loading="lazy"
                             />
                             
                             <ImageComp 
