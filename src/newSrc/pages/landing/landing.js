383f006778a01d8911c33196ff7a8b9fc06d9242
Sarah Carter <carters@transblue.org>
2023-02-24 09:41:46 -0800

    added lazy loading to new landing page components

--- src/newSrc/pages/landing/landing.js
+++ src/newSrc/pages/landing/landing.js
@@ -1,17 +1,19 @@
 import { Box, Grid, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import './landing.css';
 import Contact from "./contact";
-import Brands from "./brands";
-import About from "./about";
-import Services from "./newServices";
-import Speed from "./speed";
-import GivesBack from "./givesBack";
 import CTA from "../../components/cta/cta";
-import Footer from "../../components/footer/footer";
-import Mission from "./mission";
-import NewClients from "./newClients";
 import NavWrapper from "../../components/navbar/navWrapper";
+
+const About = lazy(() => import('./about'));
+const Brands = lazy(() => import('./brands'));
+const Services = lazy(() => import('./newServices'));
+const Speed = lazy(() => import('./speed'));
+const GivesBack = lazy(() => import('./givesBack'));
+const Footer = lazy(() => import('../../components/footer/footer'));
+const Mission = lazy(() => import('./mission'));
+const NewClients = lazy(() => import('./newClients'));
+
 
 function Landing() {
     const [screenSize, setScreenSize] = useState('small');
@@ -34,40 +36,41 @@ function Landing() {
 
     return (
         <>
-            <CTA />
-            {screenSize == 'large' &&
-                <video autoPlay loop muted>
-                    <source src='https://tbconnectstorage.blob.core.windows.net/projectimages/header.mp4' />
-                </video>
-            }
-            {screenSize === 'small' &&
-                <div id='header_mobile' />
+            <Suspense fallback={<div>Loading...</div>}>
+                <CTA />
+                {screenSize == 'large' &&
+                    <video autoPlay loop muted>
+                        <source src='https://tbconnectstorage.blob.core.windows.net/projectimages/header.mp4' />
+                    </video>
+                }
+                {screenSize === 'small' &&
+                    <div id='header_mobile' />
+                }
+                <NavWrapper page='home' />
+                <Box sx={{zIndex: 4, position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh', bgcolor: 'rgba(0,0,0,.3)', background: 'linear-gradient(90deg, rgba(0,0,0,1) 34%, rgba(0,0,0,0.7485119047619048) 61%, rgba(255,255,255,0) 100%)'}}></Box>
+                <Box sx={{zIndex: 5, position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh'}}>
+                    <Grid container height='100%'>
+                        <Grid item xs={1}>
 
-            }
-            <NavWrapper page='home' />
-            <Box sx={{zIndex: 4, position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh', bgcolor: 'rgba(0,0,0,.3)', background: 'linear-gradient(90deg, rgba(0,0,0,1) 34%, rgba(0,0,0,0.7485119047619048) 61%, rgba(255,255,255,0) 100%)'}}></Box>
-            <Box sx={{zIndex: 5, position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh'}}>
-                <Grid container height='100%'>
-                    <Grid item xs={1}>
-
+                        </Grid>
+                        <Grid item xs={10} my='auto'>
+                            <Typography variant='h1' sx={{color: 'white'}}>TRANSBLUE</Typography>
+                            <Typography variant='h1' sx={{color: 'white'}}>GENERAL CONTRACTORS</Typography>
+                            <Typography variant='h2' sx={{color: '#ed6a22'}}>Changing the world, one project at a time</Typography>
+                        </Grid>
+                        <Grid item xs={1} />
                     </Grid>
-                    <Grid item xs={10} my='auto'>
-                        <Typography variant='h1' sx={{color: 'white'}}>TRANSBLUE</Typography>
-                        <Typography variant='h1' sx={{color: 'white'}}>GENERAL CONTRACTORS</Typography>
-                        <Typography variant='h2' sx={{color: '#ed6a22'}}>Changing the world, one project at a time</Typography>
-                    </Grid>
-                    <Grid item xs={1} />
-                </Grid>
-            </Box>
-            <Contact />
-            <Mission />
-            <About />
-            <Speed />
-            <Services />
-            <GivesBack />
-            <NewClients />
-            <Brands />
-            <Footer />
+                </Box>
+                <Contact />
+                <Mission />
+                <About />
+                <Speed />
+                <Services />
+                <GivesBack />
+                <NewClients />
+                <Brands />
+                <Footer />
+            </Suspense>
         </>
     )
 }
