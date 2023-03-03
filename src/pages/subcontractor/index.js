c9afb531a480365af421c8892c4fdd19655a1d8f
Sarah Carter <carters@transblue.org>
2023-03-02 16:46:43 -0800

    added subcontractor video to subcontractor page

--- src/pages/subcontractor/index.js
+++ src/pages/subcontractor/index.js
@@ -115,8 +115,10 @@ function Subcontractor() {
                         </div>
                     </div>
                 </div>
+
                 <Growing scrollToApply={scrollToApply} />
                 <Respect scrollToApply={scrollToApply} />
+                <iframe autoplay id='subcontractor' src="https://www.youtube.com/embed/HUzz0oQMdiM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <Tools />
                 <div ref={applyRef}></div>
                 <div className='apply-wrapper'>
