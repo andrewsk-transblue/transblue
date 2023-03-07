140b9000e3d7e8fcab44725bf75cabb1b17ef0bd
Sarah Carter <carters@transblue.org>
2023-03-07 15:25:36 -0800

    recoded government three cols, snow, and services sections

--- src/newSrc/pages/government/government.js
+++ src/newSrc/pages/government/government.js
@@ -15,11 +15,11 @@ import roof from '../../images/government/roof.jpg';
 import ev from '../../images/government/ev.jpg';
 import lightrail from '../../images/government/lightrail.jpg';
 import govtBenefits from '../../images/government/govt.jpg';
+import ContactModal from '../../../components/contactModal';
 //import ev from '../../images/government/evCropped.jpg';
 import './style.css';
 
 import ReactGA from 'react-ga';
-import ContactModal from '../../../components/contactModal';
 import NavWrapper from '../../components/navbar/navWrapper';
 import { Box, Grid, Typography } from '@mui/material';
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID // YOUR_OWN_TRACKING_ID
@@ -59,21 +59,31 @@ function Government() {
                     </Box>
                 </div>
 
-                <div className='government-body'>
+                <Box sx={{width: '90%', pb: '20vh', maxWidth: '2000px', m: '0 auto'}}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} md={6}>
+                            <Typography variant='h2' sx={{fontWeight: 500, mb: 2, color: '#0b56a4'}}>WHY CHOOSE TRANSBLUE?</Typography>
+                            <ul style={{paddingLeft: '15px', marginBottom: '30px'}}>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}} >We understand the government bidding process</Typography></li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>We understand Scope of Work Requirements</Typography></li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>Minority Owned business in specific markets</Typography> </li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>Top notch communication</Typography> </li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>We understand Wage Requirements</Typography> </li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>High level of project reporting</Typography> </li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>Onsite management</Typography> </li>
+                                <li><Typography variant='body2' sx={{fontWeight: 400}}>Superior safety program with track record of 0 injuries</Typography> </li>
+                            </ul>
+                            <a className='cta' href='/locations'>FIND A LOCATION</a>
+                            <ContactModal />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <img src={govtBenefits} style={{width: '100%'}} alt='' />
+                        </Grid>
 
-                    <Benefits 
-                        img={govtBenefits}
-                        list={[
-                            'We understand the government bidding process',
-                            'We understand Scope of Work Requirements',
-                            'Minority Owned business in specific markets',
-                            'Top notch communication',
-                            'We understand Wage Requirements',
-                            'High level of project reporting',
-                            'Onsite management',
-                            'Superior safety program with track record of 0 injuries'
-                        ]}
-                    />
+                    </Grid>
+                </Box>
+
+                <Box sx={{width: '90%', pb: '20vh', maxWidth: '2000px', m: '0 auto'}}>
                     <ThreeCols 
                         card1={{
                             img: ev,
@@ -91,18 +101,55 @@ function Government() {
                             body: 'We replace and install new roofing systems for government properties, including: flat PVC and TPO, metals, asphalt shingles, green roofs, tiles, torch down, and more'
                         }}
                     />
-                    <Snow
-                        img={snow}
-                        title='GOVERNMENT SNOW &amp; ICE MANAGEMENT'
-                        p1='At Transblue, we provide comprehensive snow and ice management plans for government properties. This includes plowing, deicing, hauling, snow staging, shoveling, and icicle melting.'
-                        p2='Keeping city officials, staff and community members safe is always our goal.'
-                        p3='Send us a message today or visit our snow website to learn about the World Class Snow services we offer, and how Transblue can help you this winter.'
-                    />
+                </Box>
+
+                <Box sx={{width: '90%', py: '20vh', maxWidth: '2000px', m: '0 auto'}}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} md={5}>
+                            <Typography variant='h2' sx={{color: '#0b56a4', fontWeight: 500, mb: 2}}>GOVERNMENT SNOW &amp; ICE MANAGEMENT</Typography>
+                            <Typography variant='body2' sx={{mb: 1}}>At Transblue, we provide comprehensive snow and ice management plans for government properties. This includes plowing, deicing, hauling, snow staging, shoveling, and icicle melting.</Typography>
+                            <Typography variant='body2' sx={{mb: 1}}>Keeping city officials, staff and community members safe is always our goal.</Typography>
+                            <Typography variant='body2' sx={{mb: 4}}>Send us a message today or visit our snow website to learn about the World Class Snow services we offer, and how Transblue can help you this winter.</Typography>
+                            <a className='cta' href='https://snow.transblue.com' target='_blank' rel='noreferrer'>SNOW SERVICES</a><ContactModal />
+                        </Grid>
+                        <Grid item xs={12} md={7}>
+                            <img src={snow} alt='Multifamily Snow Removal' style={{width: '100%'}} />
+                        </Grid>
+                    </Grid>
+                </Box>
+
+                <Box sx={{width: '90%', pb: '20vh', maxWidth: '2000px', m: '0 auto'}}>
+                <Grid container spacing={5}>
+                    <Grid item xs={12} md={7}>
+                        <iframe width="100%" height="430" src="https://www.youtube.com/embed/ddYhFI8NU3w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+                    </Grid>
+                    <Grid item xs={12} md={5}>
+                        <Typography variant='h2' sx={{mb: 2, color: '#0b56a4', fontWeight: 500}}>MULTIFAMILY SERVICES</Typography>
+                        <ul style={{paddingLeft: '15px', marginBottom: '30px'}}>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Snow and Ice Management</Typography></li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Swimming Pool construction and resurfacing</Typography></li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Asphalt Paving</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Large Landscape installs</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Roofing, siding, painting</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Remodels and refreshes</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Concrete work</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Drainage systems</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Decking, fencing and railing systems</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>Electric Vehicle Charging Systems, Solar Power</Typography> </li>
+                            <li><Typography variant='body2' sx={{fontWeight: 400}}>ESG Service Offerings</Typography> </li>
+                        </ul>
+
+                        <a className='cta' href='/locations'>LOCATIONS</a><ContactModal />
+                    </Grid>
+                </Grid>
+            </Box>
+
+                <div className='government-body'>
                     <ServiceList
                         page='government'
                         title='GOVERNMENT SERVICES'
                         services={['Snow and Ice Management',
-                            'Swimming Pool construction and resurfacing',
+                            '',
                             'Asphalt Paving',
                             'Large Landscape installs',
                             'Roofing',
