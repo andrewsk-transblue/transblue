7f223d145e14cbe2577879266d8913da9ae9feb8
Sarah Carter <carters@transblue.org>
2023-03-10 15:01:02 -0800

    redirect user to old featured page

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const Government = lazy(() => import ('./newSrc/pages/government/government'));
 const Locations = lazy(() => import('./pages/locations/index'));
 const Franchise = lazy(() => import('./pages/franchise/franchise'));
 const Subcontractor = lazy(() => import('./pages/subcontractor/index'));
-const Featured = lazy(() => import('./newSrc/pages/featured/featured'));
+const Featured = lazy(() => import('./pages/featured'));
 const Featured2 = lazy(() => import('./newSrc/pages/featured/featured2'));
 const Featured3 = lazy(() => import('./newSrc/pages/featured/featured3'));
 
