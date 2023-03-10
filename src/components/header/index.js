41fcb71f2c4f9cfa91ce73905a2b672d1b2aa05d
Sarah Carter <carters@transblue.org>
2023-03-10 14:30:00 -0800

    added a Fade effect from react-reveal to header title

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,6 +3,7 @@ import FranchiseNav from '../franchiseNav';
 import './style.css';
 import military from '../../images/military.jpg';
 import militarySmall from '../../images/military-small.png';
+import Fade from 'react-reveal/Fade';
 
 function Header(props) {
     const [screenSize, setScreenSize] = useState('large');
@@ -21,7 +22,7 @@ function Header(props) {
                 {props.name !== 'locations' && <div className='overlay'></div>}
                 <div className={props.franchise === true 
                     ? 'header-text franchise-header' 
-                    : 'header-text'}>{props.title}
+                    : 'header-text'}><Fade top cascade>{props.title}</Fade>
                     <div className='header-subtext'>{props.subtitle}</div>
                 </div>
             </div>
