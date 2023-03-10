f22b0fecd122801da568110a9567d00f77a48916
Sarah Carter <carters@transblue.org>
2023-03-10 11:43:04 -0800

    added icon buttons and divider to featured projects

--- src/newSrc/pages/featured/featured2.js
+++ src/newSrc/pages/featured/featured2.js
@@ -1,23 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../../components/header/index";
 import NavWrapper from "../../components/navbar/navWrapper";
 import asphalt3 from '../../images/featured/asphalt3.jpg';
 import { motion, useAnimation } from 'framer-motion/dist/framer-motion';
 import { useInView } from "react-intersection-observer";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Icon, IconButton, Typography } from "@mui/material";
 import roofing3 from '../../images/featured/roofing3.jpg';
 import urban4 from '../../images/featured/urban4.jpg';
 import transformation3 from '../../images/featured/transformation3.jpg';
 import trendy3 from '../../images/featured/trendy3.jpg';
 import './style.css';
 import Footer from "../../components/footer/footer";
+import { Filter, PlayCircleOutline } from "@mui/icons-material";
+
+const screenWidth = window.innerWidth;
 
 const cardVariants = {
     hidden: {
-        opacity: 0
+        opacity: 0,
+        x: -400
     },
     visible: {
-        x: [-200, 0],
+        x: [-400, 0],
         transition: {
             type: "spring",
             bounce: 0.4,
@@ -37,6 +41,7 @@ const titleVariants = {
             type: "spring",
             bounce: 0.4,
             duration: 0.8,
+            delay: 0.5
         },
         opacity: [0, 1]
     }
@@ -52,6 +57,7 @@ const bodyVariants = {
             type: "spring",
             bounce: 0.4,
             duration: 0.8,
+            delay: 1
         },
         opacity: [0, 1]
     }
@@ -62,7 +68,7 @@ const overlayVariants = {
         opacity: 0,
     },
     visible: {
-        x: [-200, 0],
+        x: [-400, 0],
         transition: {
             type: "spring",
             bounce: 0.4,
@@ -121,6 +127,8 @@ function Title({ title }) {
             animate={control}
         >
             <Typography variant='h2' sx={{fontWeight: 500, letterSpacing: '.03em', color: 'white'}}>{title}</Typography>
+            <Divider sx={{width: '20%', bgcolor: '#ed6a22', mt: 2, border: '1px solid #ed6a22'}} />
+
         </motion.div>
     )
 };
@@ -142,6 +150,17 @@ function Body({ body }) {
             animate={control}
         >
             <Typography variant='body2' sx={{my: 3, letterSpacing: '.03em', color: 'whitesmoke'}}>{body}</Typography>
+
+            <IconButton
+                
+            >
+                <PlayCircleOutline sx={{color: '#ed6a22'}} fontSize='large' />
+            </IconButton>
+            <IconButton
+                
+            >
+                <Filter sx={{color: '#ed6a22'}} fontSize='large' />
+            </IconButton>
         </motion.div>
     )
 };
@@ -152,8 +171,8 @@ function Featured2() {
             <Header title='FEATURED PROJECTS' name='featured' />
             <NavWrapper />
             
-            <Box sx={{width: '100%', height: '100%', minHeight: '100vh', pt: '20vh', background: 'rgb(0,0,0)', background: 'linear-gradient(0deg, rgba(0,0,0,1) 34%, rgba(65,65,65,1) 90%)', overflowX: 'hidden'}}>
-                <Box sx={{width: '90vw', m: '0 auto'}}>
+            <Box sx={{width: '100%', height: '100%', minHeight: '100vh', pt: '20vh', background: 'rgb(0,0,0)', background: 'linear-gradient(0deg, rgba(30,30,30) 34%, rgba(65,65,65,1) 90%)', overflowX: 'hidden'}}>
+                <Box sx={{width: '90vw', m: '0 auto', maxWidth: '2000px'}}>
                     <Box sx={{minHeight: '100vh'}}>
                         <Grid container spacing={8}>
                             <Grid item xs={12} md={6} my='auto'>
