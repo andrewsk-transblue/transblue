1ec3d199dd8074e967d9d5ae59b1b6e10804fec2
Sarah Carter <carters@transblue.org>
2023-03-31 15:29:13 -0700

    reordered grid columns in recurring page on small screens

--- src/newSrc/pages/recurring/recurring.js
+++ src/newSrc/pages/recurring/recurring.js
@@ -23,22 +23,22 @@ function Recurring() {
             <Header name='recurring' title='RECURRING MAINTENANCE' />
 
             <Box sx={{width: '90%', py: '20vh', maxWidth: '2000px', m: '0 auto'}}>
-                <Grid container spacing={10}>
-                    <Grid item xs={12} md={6} my='auto'>
+                <Grid container spacing={{xs: 5, md: 10}}>
+                    <Grid item xs={12} md={6} my='auto' order={{xs: 2, md: 1}}>
                         <Typography variant='h2' sx={{color: '#0b56a4', fontWeight: 500, mb: 2}}>ON DEMAND MAINTENANCE</Typography>
                         <Typography variant='body2' sx={{mb: 2}}>We understand that unexpected maintenance issues can arise at any time, causing disruptions and impacting your business operations. That's why we offer on-demand maintenance services that can quickly address and resolve any issues as they arise. </Typography>
                         <Typography variant='body2'>
                         Our team of skilled professionals is available round the clock to offer quick and efficient maintenance services, minimizing downtime and ensuring your business continues to run smoothly. With Transblue, you can have peace of mind knowing that your business is in good hands.
                         </Typography>
                     </Grid>
-                    <Grid item xs={12} md={6} my='auto'>
+                    <Grid item xs={12} md={6} my='auto' order={{ xs: 1, md: 2}}>
                         <img src={maintenance} style={{width: '100%', borderRadius: '0px 0px 200px 0px'}} />
                     </Grid>
                 </Grid>
             </Box>
 
             <Box sx={{width: '90%', pb: '20vh', maxWidth: '2000px', m: '0 auto'}}>
-                <Grid container spacing={10}>
+                <Grid container spacing={{xs: 5, md: 10}}>
                     <Grid item xs={12} md={6} my='auto'>
                         <Box sx={{position: 'relative', width: '100%', maxWidth: '600px', height: '400px', bgcolor: '#ed6a22', borderRadius: '200px 0px', boxShadow: '0px 3px 15px rgba(0,0,0,0.5)'}}>
                             <img src={dispatch} style={{maxWidth: '400px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} />
@@ -70,7 +70,7 @@ function Recurring() {
                         <Typography variant='h2' sx={{color: '#0b56a4', mb: 2, fontWeight: 500, letterSpacing: '.03em'}}>
                             EXTENSIVE SUBCONTRACTOR NETWORK
                         </Typography>
-                        <Typography variant='body2' sx={{mb: 3}}>With an extensive network of trusted and experienced subcontractors, Transblue ensures that quality work is done at every site, giving you peace of mind knowing that your asphalt project is in good hands.</Typography>
+                        <Typography variant='body2' sx={{mb: 3}}>With an extensive network of trusted and experienced subcontractors, Transblue ensures that quality work is done at every site, giving you peace of mind knowing that your property is in good hands.</Typography>
                         <Button
                             variant='contained'
                         >
